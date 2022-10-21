bc0059db1b520180d17ca8b910189abdea741f41
Lani <laniiiiworld@gmail.com>
2022-10-21 15:22:20 +0900

    Update 삼총사.js

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,18 +1,15 @@
+//조합 응용
 function solution(number) {
-    const visited = Array(number.length).fill(false);
     let count = 0;
-    const dfs = (k, sum) => {
+    const combination = (k, sum, start) => {
         if(k === 0) {
             count += (sum === 0)? 1 : 0;
             return;
         }
-        for(let i=0; i<number.length; i++) {
-            if(visited[i]) continue;
-            visited[i] = true;
-            dfs(k-1, sum + number[i]);
-            visited[i] = false;
+        for(let i=start; i<number.length; i++) {
+            combination(k-1, sum + number[i], i+1);
         }
     };
-    dfs(3, 0);
-    return count/6;
+    combination(3, 0, 0);
+    return count;
 }
