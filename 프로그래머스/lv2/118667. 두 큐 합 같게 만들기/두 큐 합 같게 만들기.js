f17b5a7b57f7345ff79336c02f8ec6b28088224b
Lani <laniiiiworld@gmail.com>
2022-10-21 17:22:51 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 17.57 ms, Memory: 47.5 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,26 +1,28 @@
 function solution(queue1, queue2) {
-    let sum1 = queue1.reduce((a, b) => a + b)
-    let sum2 = queue2.reduce((a, b) => a + b)
-    let i1 = 0
-    let i2 = 0
-    let answer = 0
-    let maxCount = queue1.length * 2 + 2
-    while (sum1 !== sum2) {
-        if (maxCount-- === 0) {
-            return -1
-        }
-        if (sum1 < sum2) {
-            const i = i2++
-            const item = i < queue2.length ? queue2[i] : queue1[i % queue2.length]
-            sum2 -= item
-            sum1 += item
+    const maxIndex = queue1.length * 2 - 1;
+    let count = 0;
+    let sum1 = queue1.reduce((sum, num) => sum+num, 0);
+    let sum2 = queue2.reduce((sum, num) => sum+num, 0);
+    let start1 = 0;
+    let start2 = 0;
+    let firstEl = 0;
+
+    while(sum1 !== sum2) {
+        if(start1 >= maxIndex && start2 >= maxIndex) return -1;
+        count++;
+
+        if(sum1 > sum2) {
+            firstEl = queue1[start1++];
+            queue2.push(firstEl);
+            sum1 -= firstEl;
+            sum2 += firstEl;
         } else {
-            const i = i1++
-            const item = i < queue1.length ? queue1[i] : queue2[i % queue1.length]
-            sum1 -= item
-            sum2 += item
+            firstEl = queue2[start2++];
+            queue1.push(firstEl);
+            sum1 += firstEl;
+            sum2 -= firstEl;
         }
-        answer++
     }
-    return answer;
+
+    return count;
 }
