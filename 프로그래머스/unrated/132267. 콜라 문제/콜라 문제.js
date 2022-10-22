d0d5754a3f73c406694373f596a38ca76619a127
Lani <laniiiiworld@gmail.com>
2022-10-22 14:09:13 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,14 +1,12 @@
 function solution(a, b, n) {
     let count = 0;
-    let extra = 0;
-    
+
     while(n >= a) {
-        extra = n%a;
+        const extra = n%a;
         n = (n-extra)/a*b;
         count += n;
         n += extra;
-        extra = 0;
     }
-    
+
     return count;
 }
