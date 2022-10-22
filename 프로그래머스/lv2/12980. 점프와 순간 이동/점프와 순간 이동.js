2cb75d9331b724a2293a10934eb8da3ff85a85ea
Lani <laniiiiworld@gmail.com>
2022-10-22 13:22:51 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let count = 0;
+    while(n > 0) {
+        if(n%2) {
+            n -= 1;
+            count += 1;
+        } else {
+            n = n/2;
+        }
+    }
+    return count;
+}
