cde4697284836a0618cdc68fa1b050faf092c5e1
Lani <laniiiiworld@gmail.com>
2022-11-07 12:27:57 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -2,6 +2,7 @@ function solution(fees, records) {
     const answer = [];
     const [baseTime, basePrice, unitTime, unitPrice] = fees;
     const carNumbers = []; //계산된 차량 번호들을 저장하는 배열
+    
     records = records.map(record => record.split(' '));
     
     //시각(시간:분)을 입력받아 분으로 변환해주는 함수
@@ -11,27 +12,6 @@ function solution(fees, records) {
     };
     //마지막 시각. 출차되지 않은 경우 계산에 필요
     const LAST_TIME = getMinute('23:59');
-    //누적 주차시간(분)을 구하는 함수
-    const getTotalTime = (list) => {
-        let totalTime = 0;
-        for(let i=0; i<list.length; i++) {
-            const [time, , inout] = list[i];
-            if(inout === 'IN') {
-                totalTime -= getMinute(time);
-                if(i === list.length-1) {
-                    totalTime += LAST_TIME;
-                }
-            } else {
-                totalTime += getMinute(time);
-            }   
-        }
-        return totalTime;
-    };
-    //청구할 주차요금을 구하는 함수
-    const getTotalFee = (totalTime) => {
-        if(totalTime <= baseTime) return basePrice;
-        return basePrice + Math.ceil((totalTime - baseTime) / unitTime) * unitPrice;
-    };
     
     for(let i=0; i<records.length; i++) {
         const [,car,] = records[i];
@@ -39,10 +19,25 @@ function solution(fees, records) {
         carNumbers.push(car);
         
         const list = records.filter(record => record[1] === car);
-        const totalTime = getTotalTime(list);
-        const totalFee = getTotalFee(totalTime);
+        //누적 주차시간(분)
+        const totalTime = list.reduce((totalTime, item) => {
+            const [time, , inout] = item;
+            if(inout === 'IN') {
+                totalTime += (LAST_TIME - getMinute(time));
+            } else {
+                totalTime -= (LAST_TIME - getMinute(time));
+            }
+            return totalTime;
+        }, 0);
+        //청구할 주차요금
+        let totalFee = basePrice;
+        if(totalTime > baseTime) {
+            totalFee += Math.ceil((totalTime - baseTime)/unitTime) * unitPrice;
+        }
+        
         answer.push({car, totalFee});
     }
     
-    return answer.sort((a, b) => a.car - b.car).map(item => item.totalFee);
+    return answer.sort((a, b) => a.car - b.car)
+                 .map(item => item.totalFee);
 }
