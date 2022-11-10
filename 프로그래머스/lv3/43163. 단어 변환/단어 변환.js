7c18f1614d4b219127b3468e331933bc5550eafd
Lani <laniiiiworld@gmail.com>
2022-11-10 18:11:12 +0900

    [level 3] Title: 단어 변환, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -0,0 +1,36 @@
+function solution(begin, target, words) {
+    const visited = Array(words.length).fill(false);
+    let answer = words.length;
+    
+    //변환할 수 있는 단어인지 확인하는 함수
+    const isChange = (before, after) => {
+        let count = 0;
+        for(let i=0; i<before.length; i++) {
+            if(before[i] !== after[i]) continue;
+            count++;
+        }
+        //하나의 알파벳만 바뀌는지 확인
+        return count === before.length - 1;
+    };
+    
+    //dfs 탐색
+    const dfs = (word, count) => {
+        if(word === target) {
+            answer = Math.min(answer, count);
+            return;
+        }
+        
+        for(let i=0; i<words.length; i++) {
+            if(visited[i]) continue;
+            if(!isChange(word, words[i])) continue;
+            
+            visited[i] = true;
+            dfs(words[i], count+1);
+            visited[i] = false;
+        }
+    };
+    
+    dfs(begin, 0);
+    
+    return (answer === words.length) ? 0 : answer;
+}
