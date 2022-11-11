ca9fac1c3f4d293434b1f4a134a5e95747fa0d4a
Lani <laniiiiworld@gmail.com>
2022-11-11 09:10:58 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.03 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/README.md
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 파일명 정렬 - 17686 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686?language=javascript) 
 
 ### 성능 요약
 
-메모리: 34.5 MB, 시간: 2.38 ms
+메모리: 34.4 MB, 시간: 2.03 ms
 
 ### 구분
 
--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,14 +1,7 @@
 function solution(files) {
     return files.map(file => {
-                    const arr = file.split(/[\d]/g).filter(value => value);
-                    const startIndex = file.indexOf(file.replace(arr[0],''));
-                    const endIndex = arr[1]? file.indexOf(arr[1]) : file.length;
-                    
-                    const head = arr[0].toLowerCase();
-                    const number = file.slice(startIndex, endIndex);
-                    const tail = file.slice(endIndex);
-                    
-                    return [file, head, number, tail];
+                    const [fileName, head, number, tail] = file.match(/([\D]+)([0-9]+)(.*)$/);
+                    return [fileName, head.toLowerCase(), number, tail];
                 })
                 .sort((a, b) => {
                     if (a[1] > b[1]) return 1;
