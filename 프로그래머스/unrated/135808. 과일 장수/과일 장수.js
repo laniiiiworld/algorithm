4e6d14c6474d75ec5a943e024934b036c4b0373b
Lani <laniiiiworld@gmail.com>
2022-11-14 22:20:19 +0900

    [unrated] Title: 과일 장수, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,10 +1,5 @@
 function solution(k, m, score) {
-    const apples = score.sort((a, b) => b - a);
-    const stack = [];
-    
-    for(let i=m-1; i<apples.length; i+=m) {
-        stack.push(apples[i]);
-    }
-    
-    return stack.reduce((acc, cur) => acc += cur*m, 0);
+    return score.sort((a, b) => b - a)
+                .filter((v, i) => i % m === m - 1)
+                .reduce((acc, cur) => acc += cur*m, 0);
 }
