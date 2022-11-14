4010d9709c86be8c8c7ff96956c900420cf1421e
Lani <laniiiiworld@gmail.com>
2022-11-14 22:03:57 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.43 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,28 +1,15 @@
 function solution(survey, choices) {
-    const scores = [3, 2, 1, 0, 1, 2, 3];
-    const testResult = {RT: 0, CF: 0, JM: 0, AN: 0};
     const types = ['RT', 'CF', 'JM', 'AN'];
-    const plusType = ['T', 'F', 'M', 'N'];
-    
-    const setTestResult = (score, alphabet) => {
-        const type = types.find(t => t.split('').includes(alphabet));
-        if(plusType.includes(alphabet)) {
-            testResult[type] += score;
-        }else {
-            testResult[type] -= score;
-        }
-    };
+    const scores = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
     
     survey.forEach((type, index) => {
-        const checkIndex = choices[index] - 1;
-        if(checkIndex < 4) {
-            setTestResult(scores[checkIndex], type.charAt(0));
+        const choice = choices[index];
+        if(choice <= 4) {
+            scores[type.charAt(0)] += 4 - choice;
         } else {
-            setTestResult(scores[checkIndex], type.charAt(1));
+            scores[type.charAt(1)] += choice % 4;
         }
     });
     
-    return types.map(type => (testResult[type] > 0) ? type[1] : type[0])
-                .join('');
-
+    return types.map(([a, b]) => (scores[a] >= scores[b]) ? a : b).join('');
 }
