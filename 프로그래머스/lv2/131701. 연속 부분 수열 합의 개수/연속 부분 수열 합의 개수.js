22aa1058f5a90150024001f6409d57ceddea51a5
Lani <laniiiiworld@gmail.com>
2022-11-16 17:43:37 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 66.16 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,19 +1,22 @@
 function solution(elements) {
-    var answer = 0;
-    const len = elements.length;
-    elements = [...elements  , ...elements.splice(0, elements.length -1)];
-    const dp = new Array(elements.length + 1).fill(0);
-    dp[0] = 0;
-    for(let i = 0; i < elements.length; i++){
-        dp[i + 1] += dp[i] + elements[i];
+    const base = [...elements, ...elements];
+    const sums = new Set();
+    
+    const dp = new Array(base.length+1).fill(0);
+    for(let i = 0; i < base.length; i++){
+        dp[i + 1] += dp[i] + base[i];
     }
-    const set = new Set();
-    set.add(dp[len]);
-    for(let j = 1; j < len; j++){
-        for(let i = 1; i < dp.length; i++){
-            if(i + j >= dp.length) continue;
-            set.add(dp[j + i] - dp[i]);        
+    
+    sums.add(dp[dp.length - 1]);
+    
+    let count = 1;
+    while(count < elements.length) {
+        for(let i=0; i<elements.length; i++) {
+            if(count + i >= dp.length) continue;
+            sums.add(dp[count + i] - dp[i]);
         }
+        count++;
     }
-    return set.size;
+    
+    return sums.size;
 }
