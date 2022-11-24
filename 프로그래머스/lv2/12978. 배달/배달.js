eda23950b4e92c8470e41330d4baf68abf3371c1
Lani <laniiiiworld@gmail.com>
2022-11-24 21:14:05 +0900

    [level 2] Title: 배달, Time: 0.73 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.js
+++ 프로그래머스/lv2/12978. 배달/배달.js
@@ -1,23 +1,37 @@
 function solution(N, road, K) {
-    const distance = new Array(N).fill(0).map((row, idx) => new Array(N).fill(Infinity).map((v, i) => i === idx? 0 : Infinity));
+    const visited = new Array(N).fill(false);
+    const distance = new Array(N).fill(Infinity);
+    distance[0] = 0;
     
-    //각 간선에 대한 정보 초기화
-    road.forEach(([a,b,c]) => {
-        const min = Math.min(a, b) - 1;
-        const max = Math.max(a, b) - 1;
-        distance[min][max] = Math.min(distance[min][max], c);
-        distance[max][min] = Math.min(distance[max][min], c);
-        // distance[a-1][b-1] = Math.min(distance[a-1][b-1], c);
-    });
-    
-    //플로이드 워셜 알고리즘 수행
-    for(let k=0; k<N; k++) {
-        for(let a=0; a<N; a++) {
-            for(let b=0; b<N; b++) {
-                const calcDistance = distance[a][k] + distance[k][b];
-                distance[a][b] = Math.min(distance[a][b], calcDistance);
-            }
+    const getSmallestNode = () => {
+        let minCost = Infinity;
+        let node = -1;
+        for(let i=0; i<distance.length; i++) {
+            if(visited[i] || distance[i] >= minCost) continue;
+            minCost = distance[i];
+            node = i+1;
         }
+        return node;
+    };
+    
+    while(visited.findIndex(value => !value) > -1){
+        const node = getSmallestNode();
+        
+        visited[node-1] = true;
+        
+        road.filter(item => item[0] === node || item[1] === node).forEach(([a, b, c]) => {
+            const calcCost = distance[node-1] + c;
+            if(a === node) {
+                if(calcCost < distance[b-1]) {
+                    distance[b-1] = calcCost;
+                }
+            } else {
+                if(calcCost < distance[a-1]) {
+                    distance[a-1] = calcCost;
+                }
+            }
+        });
     }
-    return distance[0].filter(value => value <= K).length;
+    
+    return distance.filter(value => value <= K).length;
 }
