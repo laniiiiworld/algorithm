b128bff6d3022591eb3fb1a559a444b02009fcb2
Lani <laniiiiworld@gmail.com>
2023-01-19 20:26:09 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,21 +1,13 @@
 function solution(ingredient) {
     let count = 0;
     const stack = [];
-    const isHamberger = (items) => {
-        const hamberger = [1, 2, 3, 1];
-        for(let i=0; i<hamberger.length; i++) {
-            if(items[i] !== hamberger[i]) return false;
-        }
-        return true;
-    };
     
     for(let i=0; i<ingredient.length; i++) {
         const value = ingredient[i];
         stack.push(value);
         if(stack.length < 4 || value !== 1) continue;
         
-        const items = stack.slice(-4);
-        if(isHamberger(items)) {
+        if(stack.slice(-4).join('') === '1231') {
             count++;
             stack.splice(-4);
             continue;
