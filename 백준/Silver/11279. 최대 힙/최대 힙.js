16e23f8e6b77550d1f4beaf0eba1a45d97280a23
Lani <laniiiiworld@gmail.com>
2023-01-23 19:43:33 +0900

    Update 최대 힙.js

--- 백준/Silver/11279. 최대 힙/최대 힙.js
+++ 백준/Silver/11279. 최대 힙/최대 힙.js
@@ -1,78 +1,64 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const N = Number(input[0]);
-const commands = input.slice(1, 1 + N).map(Number);
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 class MaxHeap {
   constructor() {
-    this.heap = [];
+    this.heap = [null];
   }
-  empty() {
-    return this.heap.length === 0 ? true : false;
-  }
-  insert(value) {
+  push(value) {
     this.heap.push(value);
-    this.bubbleUp();
-  }
-  bubbleUp() {
     let currentIndex = this.heap.length - 1;
-    while (currentIndex > 0) {
-      const parentIndex = Math.floor((currentIndex - 1) / 2);
-      if (this.heap[parentIndex] >= this.heap[currentIndex]) break;
-      [this.heap[currentIndex], this.heap[parentIndex]] = [
-        this.heap[parentIndex],
-        this.heap[currentIndex],
-      ];
+    let parentIndex = Math.floor(currentIndex / 2);
+
+    while (parentIndex !== 0 && this.heap[parentIndex] < value) {
+      [this.heap[parentIndex], this.heap[currentIndex]] = [this.heap[currentIndex], this.heap[parentIndex]];
+
       currentIndex = parentIndex;
+      parentIndex = Math.floor(currentIndex / 2);
     }
   }
+
   pop() {
-    if (this.heap.length === 1) return this.heap.pop();
-    const max = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.bubbleDown(0);
-    return max;
-  }
-  bubbleDown(index) {
-    const leftNodeIndex = 2 * index + 1;
-    const rightNodeIndex = 2 * index + 2;
-    const length = this.heap.length;
-    let maxIndex = index;
-    if (
-      leftNodeIndex < length &&
-      this.heap[leftNodeIndex] > this.heap[maxIndex]
-    ) {
-      maxIndex = leftNodeIndex;
+    if (this.heap.length === 2) return this.heap.pop();
+
+    const returnValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+
+    while (this.heap[currentIndex] < this.heap[leftIndex] || this.heap[currentIndex] < this.heap[rightIndex]) {
+      if (this.heap[leftIndex] < this.heap[rightIndex]) {
+        [this.heap[rightIndex], this.heap[currentIndex]] = [this.heap[currentIndex], this.heap[rightIndex]];
+        currentIndex = rightIndex;
+      } else {
+        [this.heap[leftIndex], this.heap[currentIndex]] = [this.heap[currentIndex], this.heap[leftIndex]];
+        currentIndex = leftIndex;
+      }
+      leftIndex = currentIndex * 2;
+      rightIndex = currentIndex * 2 + 1;
     }
-    if (
-      rightNodeIndex < length &&
-      this.heap[rightNodeIndex] > this.heap[maxIndex]
-    ) {
-      maxIndex = rightNodeIndex;
-    }
-    if (maxIndex !== index) {
-      [this.heap[index], this.heap[maxIndex]] = [
-        this.heap[maxIndex],
-        this.heap[index],
-      ];
-      this.bubbleDown(maxIndex);
-    }
+
+    return returnValue;
   }
 }
 
-const solution = (N, commands) => {
-  const maxHeap = new MaxHeap();
+function solution(input) {
   let answer = '';
-  commands.forEach((command) => {
-    if (command === 0) {
-      if (maxHeap.empty()) answer += '0\n';
-      else answer += maxHeap.pop() + '\n';
+  const heap = new MaxHeap();
+  for (const v of input) {
+    if (v === 0) {
+      if (heap.heap.length === 1) {
+        answer += '0\n';
+      } else {
+        answer += `${heap.pop()}\n`;
+      }
     } else {
-      maxHeap.insert(command);
+      heap.push(v);
     }
-  });
-  return answer;
-};
+  }
 
-console.log(solution(N, commands));
+  return answer;
+}
+
+console.log(solution(input.slice(1).map(Number)));
