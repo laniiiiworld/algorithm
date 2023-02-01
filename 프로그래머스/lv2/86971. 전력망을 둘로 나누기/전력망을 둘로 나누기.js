7a001dd7194a7820db157b8c4982f75d3dde92d9
Lani <laniiiiworld@gmail.com>
2023-02-01 15:37:44 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 17.90 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,39 +1,43 @@
 function solution(n, wires) {
     let answer = n;
-    let visited = Array(n).fill(false);
+    let visited = Array(n+1).fill(false);
     //전달받은 송전탑과 연결된 송전탑의 개수를 구하는 함수
     const getNewTowerCount = (splicedWires, tower) => {
         let towerCount = 0;
         const stack = [tower];
         while(stack.length) {
-            const now = stack.pop();
-            if(!visited[now-1]) towerCount++;
-            visited[now-1] = true;
+            const now = stack.shift();
+            if(!visited[now]) towerCount++;
+            visited[now] = true;
             for(let i=0; i<splicedWires.length; i++) {
-                if(splicedWires[i][0] !== now && splicedWires[i][1] !== now) {
+                const [start, end] = splicedWires[i];
+                if(start !== now && end !== now) {
                     continue;
                 }
-                if(!visited[splicedWires[i][1] - 1]) stack.push(splicedWires[i][1]);
-                if(!visited[splicedWires[i][0] - 1]) stack.push(splicedWires[i][0]);
+                if(!visited[end]) stack.push(end);
+                if(!visited[start]) stack.push(start);
             }
         }
         return towerCount;
     };
-    
+
     for(let i=0; i<wires.length; i++) {
         visited = visited.map(value => false);
         const splicedWires = [...wires]; //전선들 중 하나가 끊어진 전선 정보
         splicedWires.splice(i,1);
+        
         const count = [0, 0];
         let index = 0;
+        
         for(let tower=1; tower<=n; tower++) {
-            if(visited[tower-1]) continue;
+            if(visited[tower]) continue;
             count[index] = getNewTowerCount(splicedWires, tower);
             index++;
         }
+        
         //각 전력망의 송전탑 개수 차이가 더 줄어들었는지 비교
         answer = Math.min(answer, Math.abs(count[0] - count[1]));
     }
-    
+
     return answer;
 }
