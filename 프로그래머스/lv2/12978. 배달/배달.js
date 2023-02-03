61a88fd6a0b0f407e14f4255f7710fffc6b95991
Lani <laniiiiworld@gmail.com>
2023-02-03 13:05:39 +0900

    [level 2] Title: 배달, Time: 4.15 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.js
+++ 프로그래머스/lv2/12978. 배달/배달.js
@@ -1,37 +1,28 @@
 function solution(N, road, K) {
-    const visited = new Array(N).fill(false);
-    const distance = new Array(N).fill(Infinity);
-    distance[0] = 0;
-    
-    const getSmallestNode = () => {
-        let minCost = Infinity;
-        let node = -1;
-        for(let i=0; i<distance.length; i++) {
-            if(visited[i] || distance[i] >= minCost) continue;
-            minCost = distance[i];
-            node = i+1;
-        }
-        return node;
-    };
-    
-    while(visited.findIndex(value => !value) > -1){
-        const node = getSmallestNode();
-        
-        visited[node-1] = true;
-        
-        road.filter(item => item[0] === node || item[1] === node).forEach(([a, b, c]) => {
-            const calcCost = distance[node-1] + c;
-            if(a === node) {
-                if(calcCost < distance[b-1]) {
-                    distance[b-1] = calcCost;
-                }
-            } else {
-                if(calcCost < distance[a-1]) {
-                    distance[a-1] = calcCost;
-                }
+    const distance = Array.from({length: N+1}, (_, idx) => {
+                        return Array.from({length: N+1}, (v, i) => i === idx? 0 : Infinity);
+                    });
+
+    //각 간선에 대한 정보 초기화
+    road.forEach(([start, end, cost]) => {
+        const min = Math.min(start, end);
+        const max = Math.max(start, end);
+        distance[min][max] = Math.min(distance[min][max], cost);
+        distance[max][min] = Math.min(distance[max][min], cost);
+    });
+
+    //플로이드 워셜 알고리즘
+    // start: 출발하는 마을
+    // mid: 거쳐가는 마을
+    // end: 도착하는 마을
+    for(let mid=1; mid<=N; mid++) {
+        for(let start=1; start<=N; start++) {
+            for(let end=1; end<=N; end++) {
+                const calcDistance = distance[start][mid] + distance[mid][end];
+                distance[start][end] = Math.min(distance[start][end], calcDistance);
             }
-        });
+        }
     }
     
-    return distance.filter(value => value <= K).length;
+    return distance[1].filter(value => value <= K).length;
 }
