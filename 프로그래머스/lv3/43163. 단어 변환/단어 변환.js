ae0d77b21d011044414f55f8ecc3d105b214e973
Lani <laniiiiworld@gmail.com>
2023-02-05 16:53:34 +0900

    [level 3] Title: 단어 변환, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,31 +1,24 @@
 function solution(begin, target, words) {
-    if(!words.includes(target)) return 0;
+    if(!words.find(word => word === target)) return 0;
     
-    const visited = Array(words.length).fill(false);
     let answer = words.length;
-    
-    //변환할 수 있는 단어인지 확인하는 함수
-    const isChange = (before, after) => {
+    const visited = new Array(words.length).fill(false);
+    const isChange = (a, b) => {
         let count = 0;
-        for(let i=0; i<before.length; i++) {
-            if(before[i] !== after[i]) continue;
-            count++;
+        for(let i=0; i<a.length; i++) {
+            if(count > 1) return false;
+            count += (a[i] !== b[i])? 1 : 0;
         }
-        //하나의 알파벳만 바뀌는지 확인
-        return count === before.length - 1;
+        return count === 1;
     };
-    
-    //dfs 탐색
-    const dfs = (word, count) => {
-        if(word === target) {
+    const dfs = (before, count) => {
+        if(before === target) {
             answer = Math.min(answer, count);
             return;
         }
         
         for(let i=0; i<words.length; i++) {
-            if(visited[i]) continue;
-            if(!isChange(word, words[i])) continue;
-            
+            if(visited[i] || !isChange(before, words[i])) continue;
             visited[i] = true;
             dfs(words[i], count+1);
             visited[i] = false;
