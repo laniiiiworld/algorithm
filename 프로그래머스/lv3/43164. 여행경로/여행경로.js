328da2a2388b5fa600ab3a8263bb2cb40ebd4305
Lani <laniiiiworld@gmail.com>
2023-02-05 16:30:25 +0900

    [level 3] Title: 여행경로, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.js
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -1,41 +1,24 @@
+// 출처: https://leetcode.com/problems/reconstruct-itinerary/solutions/437594/super-easy-and-clean-javascript-greedy-dfs-with-detailed-explanations/?languageTags=javascript
 function solution(tickets) {
-    const visited = new Array(tickets.length).fill(false);
-    const citiesMap = new Map();
-    let path = ['ICN'];
+    const answer = [];
+    const citiesMap = {};
+    const makeCourse = (node) => {
+        const destinations = citiesMap[node];
+        while(destinations && destinations.length) {
+            makeCourse(destinations.shift());
+        }
+        answer.push(node);
+    };
     
-    tickets.forEach((ticket)=>{
-        const cities = tickets.filter(([start, end]) => start === ticket[0])
-                              .map(([start, end], i) => `${end}${i}`)
-                              .sort();
-        citiesMap.set(ticket[0], cities);
+    tickets.forEach(([start, end]) => {
+        if(!citiesMap[start]) citiesMap[start] = [];
+        citiesMap[start].push(end);
     });
     
-    const dfs = (from) => {
-        const cities = citiesMap.get(from)? citiesMap.get(from) : [];
-        const beforePath = [...path];
-
-        for(let i = 0; i < cities.length; i++) {
-            const to = cities[i].replace(/[0-9]/g, '');
-            //아직 사용 안한 티켓 index
-            const index = tickets.findIndex((item, idx) => {
-                return item[0] === from && item[1] === to && !visited[idx];
-            });
-
-            if(index === -1) continue;
-            
-            visited[index] = true;
-            path.push(to);
-            
-            if (dfs(to)) break;
-            
-            path = beforePath;
-            visited[index] = false;
-        }
-        
-        return visited.every(value => value);
-    }
-
-    dfs('ICN');
-
-    return path;
+    for(const city in citiesMap) {
+        citiesMap[city].sort();
+    }  
+    
+    makeCourse('ICN');
+    return answer.reverse();
 }
