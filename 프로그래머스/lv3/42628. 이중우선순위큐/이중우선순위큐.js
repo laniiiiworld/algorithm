e21f0ba6b3a23c495fb7de757e0383746189dc0d
Lani <laniiiiworld@gmail.com>
2023-02-07 14:16:55 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,22 +1,22 @@
 function solution(operations) {
     const numbers = [];
-    
+
     operations.forEach(operation => {
-        const item = operation.split(' ');
-        
-        if(item[0] === 'I') {
-            numbers.push(Number(item[1])); //주어진 숫자를 삽입
+        const [code, value] = operation.split(' ');
+
+        if(code === 'I') {
+            numbers.push(Number(value)); //주어진 숫자를 삽입
             numbers.sort((a, b) => b - a);
         } else {
-            if(item[1] === '1') {
+            if(value === '1') {
                 numbers.shift(); //최댓값을 삭제
             } else {
                 numbers.pop();   //최솟값을 삭제
             }
         }
     });
-    
+
     if(numbers.length === 1) return [numbers[0], numbers[0]];
-    
+
     return numbers.length? [numbers[0], numbers.pop()] : [0, 0];
 }
