49da1ea8628513fe0875ce538aa7d3f46b4941cc
Lani <laniiiiworld@gmail.com>
2023-02-09 16:41:16 +0900

    [level 3] Title: 입국심사, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/README.md
+++ 프로그래머스/lv3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.26 ms
+메모리: 33.4 MB, 시간: 0.20 ms
 
 ### 구분
 
--- 프로그래머스/lv3/43238. 입국심사/입국심사.js
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.js
@@ -4,15 +4,12 @@ function solution(n, times) {
     
     while(left <= right) {
         const mid = Math.floor((left + right) / 2);
-        //mid 시간동안 심사받을 수 있는 입국자 수의 합
-        const sum = times.reduce((sum, time) => sum += Math.floor(mid/time), 0);
-        
-        if(sum < n) {
-            left = mid + 1;
-        } else {
+        const count = times.reduce((sum, time) => sum + Math.floor(mid / time), 0);
+        if(n <= count) {
             right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
-    
     return left;
 }
