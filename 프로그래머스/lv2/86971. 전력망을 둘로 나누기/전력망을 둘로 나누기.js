383758f75a7de44c30f5f74dd6dfbadc4ac47fe6
Lani <laniiiiworld@gmail.com>
2023-02-10 13:39:47 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.04 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,43 +1,35 @@
 function solution(n, wires) {
     let answer = n;
-    let visited = Array(n+1).fill(false);
-    //전달받은 송전탑과 연결된 송전탑의 개수를 구하는 함수
-    const getNewTowerCount = (splicedWires, tower) => {
-        let towerCount = 0;
-        const stack = [tower];
-        while(stack.length) {
-            const now = stack.shift();
-            if(!visited[now]) towerCount++;
-            visited[now] = true;
-            for(let i=0; i<splicedWires.length; i++) {
-                const [start, end] = splicedWires[i];
-                if(start !== now && end !== now) {
-                    continue;
-                }
-                if(!visited[end]) stack.push(end);
-                if(!visited[start]) stack.push(start);
+    const getNewTowerCount = (splicedWires) => {
+        let count = 1;
+        const distance = new Array(n+1).fill(0);
+        const gragh = Array.from({length: n+1}, () => []);
+        
+        for(const [start, end] of splicedWires) {
+            gragh[start].push(end);
+            gragh[end].push(start);
+        }
+        
+        const queue = [1];
+        distance[1] = 1;
+        while(queue.length) {
+            const start = queue.shift();
+            for(const end of gragh[start]) {
+                if(distance[end] > 0) continue;
+                distance[end] = distance[start] + 1;
+                queue.push(end);
+                count += 1;
             }
         }
-        return towerCount;
+        
+        return count;
     };
-
+    
     for(let i=0; i<wires.length; i++) {
-        visited = visited.map(value => false);
-        const splicedWires = [...wires]; //전선들 중 하나가 끊어진 전선 정보
-        splicedWires.splice(i,1);
-        
-        const count = [0, 0];
-        let index = 0;
-        
-        for(let tower=1; tower<=n; tower++) {
-            if(visited[tower]) continue;
-            count[index] = getNewTowerCount(splicedWires, tower);
-            index++;
-        }
-        
-        //각 전력망의 송전탑 개수 차이가 더 줄어들었는지 비교
-        answer = Math.min(answer, Math.abs(count[0] - count[1]));
+        const splicedWires = [...wires.slice(0, i), ...wires.slice(i+1)];
+        const count = getNewTowerCount(splicedWires);
+        answer = Math.min(answer, Math.abs(count - (n - count)));
     }
-
+    
     return answer;
 }
