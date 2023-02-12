95b2af5e4cd172a40cfc1db6d5eb18e7e2f48909
Lani <laniiiiworld@gmail.com>
2023-02-12 15:38:25 +0900

    [level 3] Title: 순위, Time: 25.05 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49191. 순위/순위.js
@@ -0,0 +1,28 @@
+function solution(n, results) {
+    let answer = 0;
+    const gragh = Array.from({length: n+1}, () => ({win:new Set(), lose:new Set()}));
+    results.forEach(([won, lost]) => {
+        gragh[won].win.add(lost);
+        gragh[lost].lose.add(won);
+    });
+    
+    for(let player=1; player<=n; player++) {
+        for(const lost of gragh[player].win) {
+            for(const p of gragh[player].lose) {
+                gragh[lost].lose.add(p);
+            }
+        }
+        for(const won of gragh[player].lose) {
+            for(const p of gragh[player].win) {
+                gragh[won].win.add(p);
+            }
+        }
+    }
+    
+    for(const player of gragh) {
+        const count = player.win.size + player.lose.size;
+        answer += (count === n-1)? 1 : 0;
+    }
+    
+    return answer;
+}
