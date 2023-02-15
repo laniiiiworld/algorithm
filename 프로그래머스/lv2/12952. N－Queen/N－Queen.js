a96b0b0989c80e1da41ca33fdad888f9b012d486
Lani <laniiiiworld@gmail.com>
2023-02-15 16:00:24 +0900

    [level 2] Title: N-Queen, Time: 351.23 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.js
@@ -0,0 +1,25 @@
+function solution(n) {
+    let count = 0;
+    const queen = Array(n).fill(0); //index : 행, queen[index] : 열
+    const check = (row) => {
+        for(let i=0; i<row; i++) {
+            if(queen[i] === queen[row] || Math.abs(queen[i] - queen[row]) === (row - i)) {
+                return false;
+            }
+        }
+        return true;
+    };
+    const search = (row) => {
+        if(row === n) count += 1;
+        
+        for(let col=0; col<n; col++) {
+            queen[row] = col;
+            if(!check(row)) continue;
+            search(row+1);
+        }
+    };
+    
+    search(0);
+    
+    return count;
+}
