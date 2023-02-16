2be67bcebf8a8d185c3bdd51a2b1063b42c4724a
Lani <laniiiiworld@gmail.com>
2023-02-16 20:20:35 +0900

    [level 3] Title: 가장 먼 노드, Time: 74.53 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,23 +1,23 @@
 function solution(n, edge) {
     const distance = Array(n+1).fill(0);
-    const gragh = Array.from({length: n+1}, () => []);
-    edge.forEach(([start, end]) => {
-        gragh[start].push(end);
-        gragh[end].push(start);
-    });
-    
-    const queue = [1];
     distance[1] = 1;
     
+    const gragh = Array.from({length: n+1}, () => []);
+    for(const [start, end] of edge) {
+        gragh[start].push(end);
+        gragh[end].push(start);
+    }
+    
+    const queue = [1];
     while(queue.length) {
-        const current = queue.shift();        
-        
+        const current = queue.shift();
         for(const node of gragh[current]) {
-            if(distance[node] > 0) continue;
-            queue.push(node);
+            if(distance[node]) continue;
             distance[node] = distance[current] + 1;
+            queue.push(node);
         }
     }
+    
     const max = Math.max(...distance);
     return distance.filter(value => value === max).length;
 }
