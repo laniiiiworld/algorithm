d957c20b6178b0a053407f19e09a7492861e10eb
Lani <laniiiiworld@gmail.com>
2023-02-18 13:04:34 +0900

    [level 2] Title: 타겟 넘버, Time: 1.12 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,15 +1,16 @@
 function solution(numbers, target) {
-    let count = 0;
-    const makeTargetNumber = (index, sum) => {
+    let answer = 0;
+    const makeNumber = (sum, index) => {
         if(index === numbers.length) {
-            count += (sum === target)? 1 : 0;
+            answer += (sum === target)? 1 : 0;
             return;
         }
-        makeTargetNumber(index + 1, sum + numbers[index]);
-        makeTargetNumber(index + 1, sum - numbers[index]);
+        
+        makeNumber(sum + numbers[index], index + 1);
+        makeNumber(sum - numbers[index], index + 1);
     };
     
-    makeTargetNumber(0, 0);
+    makeNumber(0, 0);
     
-    return count;
+    return answer;
 }
