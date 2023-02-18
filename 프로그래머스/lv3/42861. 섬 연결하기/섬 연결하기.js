e065c8172d5278809db6e29293338634240371ee
Lani <laniiiiworld@gmail.com>
2023-02-18 11:12:42 +0900

    [level 3] Title: 섬 연결하기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,33 +1,33 @@
 function solution(n, costs) {
-  const costsVisited = new Array(costs.length).fill(false); //costs[i] 확인 여부
-  costs.sort((a, b) => a[2] - b[2]); //비용이 적은 순서대로 정렬
+    let answer = 0;
+    const parents = Array.from({length: n}, (_, i) => i);
 
-  //가장 비용이 적은 곳부터 방문 처리
-  const visited = new Array(n).fill(false); //노드 방문 여부
-  visited[costs[0][0]] = true;
-  visited[costs[0][1]] = true;
-  costsVisited[0] = true; //costs[i] 확인 처리
+    const find = (x) => {
+        if(parents[x] === x) return x;
+        return parents[x] = find(parents[x]);
+    };
+    const union = (a, b) => {
+        a = find(a);
+        b = find(b);
+        if(a < b) {
+            parents[b] = a;
+        } else {
+            parents[a] = b;
+        }
+    };
+    const compare = (a, b) => {
+        a = find(a);
+        b = find(b);
+        return a === b;
+    };
 
-  let totalCost = costs[0][2]; //모든 섬이 서로 통행 가능하도록 만들 때 필요한 최소 비용
+    costs.sort((a, b) => a[2] - b[2]);
 
-  //모든 노드가 연결되는 시점까지 확인
-  while (visited.includes(false)) {
-    for (let i = 1; i < costs.length; i++) {
-      if (costsVisited[i]) continue; //확인되지 않은 costs[i]만 다리 연결 가능여부 확인
-      const [start, end, cost] = costs[i];
-      //다리를 연결할 수 있는 경우(= 둘 중 한 노드가 아직 방문처리 되지 않은 경우)
-      if ((!visited[start] && visited[end]) || (visited[start] && !visited[end])) {
-        //노드 방문 처리
-        visited[start] = true;
-        visited[end] = true;
-        //비용 추가
-        totalCost += cost;
-        //costs[i] 확인 처리
-        costsVisited[i] = true;
-        break;
-      }
+    for(const [a, b, cost] of costs) {
+        if(compare(a, b)) continue;
+        answer += cost;
+        union(a, b);
     }
-  }
 
-  return totalCost;
+    return answer;
 }
