d2d1e1112bfe4216fd6c2117dad1974ff038580d
Lani <laniiiiworld@gmail.com>
2023-02-18 22:11:46 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.59 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -2,20 +2,20 @@ function solution(maps) {
     const n = maps.length - 1;
     const m = maps[0].length - 1;
     const queue = [[0, 0, 1]];
-
+    
     while(queue.length) {
         const [row, col, count] = queue.shift();
-
-        if(row === n && col === m) return count;
+        
         if(!maps[row][col]) continue;
-
+        if(row === n && col === m) return count;
+        
         maps[row][col] = 0;
-
+        
         if(row+1 <= n) queue.push([row+1, col, count+1]);
         if(col+1 <= m) queue.push([row, col+1, count+1]);
         if(row-1 >= 0) queue.push([row-1, col, count+1]);
         if(col-1 >= 0) queue.push([row, col-1, count+1]);
     }
-
+    
     return -1;
 }
