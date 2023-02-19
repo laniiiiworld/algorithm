c52938242e56a8184dda4ad7897e77fa2d946cad
Lani <laniiiiworld@gmail.com>
2023-02-19 18:06:14 +0900

    [level 3] Title: 섬 연결하기, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,33 +1,30 @@
 function solution(n, costs) {
     let answer = 0;
-    const parents = Array.from({length: n}, (_, i) => i);
-
+    const parents = Array.from({length: n}, (_,i) => i);
     const find = (x) => {
         if(parents[x] === x) return x;
         return parents[x] = find(parents[x]);
     };
     const union = (a, b) => {
-        a = find(a);
-        b = find(b);
-        if(a < b) {
-            parents[b] = a;
+        const parentA = find(a);
+        const parentB = find(b);
+        if(parentA < parentB) {
+            parents[parentB] = parentA;
         } else {
-            parents[a] = b;
+            parents[parentA] = parentB;
         }
     };
     const compare = (a, b) => {
-        a = find(a);
-        b = find(b);
-        return a === b;
+        return find(a) === find(b);
     };
-
+    
     costs.sort((a, b) => a[2] - b[2]);
-
+    
     for(const [a, b, cost] of costs) {
         if(compare(a, b)) continue;
         answer += cost;
         union(a, b);
     }
-
+    
     return answer;
 }
