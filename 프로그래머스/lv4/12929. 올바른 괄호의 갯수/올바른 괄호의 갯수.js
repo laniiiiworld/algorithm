95b85fa0426a006e0b20516495040b56638dc060
Lani <laniiiiworld@gmail.com>
2023-02-24 11:35:53 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.js
@@ -0,0 +1,14 @@
+//카탈란 수 알고리즘
+function solution(n) {
+    const dp = new Array(n+1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    
+    for(let i = 2; i<=n; i++){
+        for(let j=1; j<=i; j++){
+            dp[i] += dp[i-j] * dp[j-1];
+        }
+    }
+    
+    return dp[n];
+}
