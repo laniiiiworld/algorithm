810c8891a869485da51bd66ff6beab70c775dfcb
Lani <laniiiiworld@gmail.com>
2023-02-26 21:13:46 +0900

    [level 2] Title: 예상 대진표, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,9 +1,11 @@
-function solution(n,a,b) {
-    let round = 0;
-    while(a !== b) {
+function solution(n, a, b) {
+    let round = 1;
+    
+    while(Math.ceil(a/2) !== Math.ceil(b/2)) {
         a = Math.ceil(a/2);
         b = Math.ceil(b/2);
-        round++;
+        round += 1;
     }
+    
     return round;
 }
