fac5da2ed9021b4a3f1cd6355be38a334b25bd51
Lani <laniiiiworld@gmail.com>
2023-02-27 22:54:01 +0900

    [unrated] Title: 귤 고르기, Time: 69.41 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,17 +1,75 @@
+class MaxHeap {
+    constructor() {
+        this.heap = [null];
+    }
+    push(item) {
+        this.heap.push(item);
+        
+        let currentIndex = this.heap.length - 1;
+        let parentIndex = Math.floor(currentIndex / 2);
+        
+        while(currentIndex > 1 && this.heap[parentIndex].count < this.heap[currentIndex].count) {
+            this._swap(currentIndex, parentIndex);
+            currentIndex = parentIndex;
+            parentIndex = Math.floor(currentIndex / 2);
+        }
+    }
+    pop() {
+        if(this.heap.length === 1) return null;
+        if(this.heap.length === 2) return this.heap.pop();
+        
+        const returnItem = this.heap[1];
+        this.heap[1] = this.heap.pop();
+        
+        let currentIndex = 1;
+        let leftIndex = currentIndex * 2;
+        let rightIndex = currentIndex * 2 + 1;
+        
+        if(!this.heap[leftIndex]) return returnItem;
+        if(!this.heap[rightIndex]) {
+            if(this.heap[currentIndex].count < this.heap[leftIndex].count) {
+                this._swap(currentIndex, leftIndex);
+            }
+            return returnItem;
+        }
+        
+        while(
+              (this.heap[currentIndex].count < this.heap[leftIndex].count) || 
+              (this.heap[currentIndex].count < this.heap[rightIndex]?.count)
+             ) {
+            const maxIndex = (this.heap[leftIndex].count < this.heap[rightIndex]?.count)? rightIndex : leftIndex;
+            this._swap(currentIndex, maxIndex);
+            
+            currentIndex = maxIndex;
+            leftIndex = currentIndex * 2;
+            rightIndex = currentIndex * 2 + 1;
+            
+            if(leftIndex >= this.heap.length) break;
+        }
+        return returnItem;
+    }
+    _swap(a, b) {
+        [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+    }
+}
+
 function solution(k, tangerine) {
+    const sizes = new Map();
+    const heap = new MaxHeap();
+    
+    for(const size of tangerine) {
+        sizes.set(size, sizes.get(size) + 1 || 1);
+    }
+    
+    for(const size of sizes.keys()) {
+        heap.push({size, count: sizes.get(size)});
+    }
+    
     let answer = 0;
     let count = 0;
-    const types = new Map();
-    
-    tangerine.forEach(v => {
-        types.set(v, (types.get(v)||0) + 1);
-    });
-    
-    const arr = [...types].sort((a, b) => b[1] - a[1]);
-    for(let i=0; i<arr.length; i++) {
-        count += arr[i][1];
-        answer++;
-        if(count >= k) break;
+    while(count < k) {
+        count += heap.pop().count;
+        answer += 1;
     }
     
     return answer;
