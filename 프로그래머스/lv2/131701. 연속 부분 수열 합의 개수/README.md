16d11b37a106d6ed65b3f81cb450172f2eacaa8a
Lani <laniiiiworld@gmail.com>
2023-02-28 16:55:18 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 796.96 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 57 MB, 시간: 66.16 ms
+메모리: 53.1 MB, 시간: 796.96 ms
 
 ### 구분
 
--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,22 +1,11 @@
 function solution(elements) {
-    const base = [...elements, ...elements];
-    const sums = new Set();
-    
-    const dp = new Array(base.length+1).fill(0);
-    for(let i = 0; i < base.length; i++){
-        dp[i + 1] += dp[i] + base[i];
-    }
-    
-    sums.add(dp[dp.length - 1]);
-    
-    let count = 1;
-    while(count < elements.length) {
+    const sums = new Set(elements);
+    const numbers = [...elements, ...elements];
+    for(let length=2; length<=elements.length; length++) {
         for(let i=0; i<elements.length; i++) {
-            if(count + i >= dp.length) continue;
-            sums.add(dp[count + i] - dp[i]);
+            const sum = numbers.slice(i, i+length).reduce((sum, cur) => sum += cur);
+            sums.add(sum);
         }
-        count++;
     }
-    
     return sums.size;
 }
