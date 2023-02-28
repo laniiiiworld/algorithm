d9b686b2e212310820f23654d82dce413eead694
Lani <laniiiiworld@gmail.com>
2023-03-01 07:05:13 +0900

    [level 3] Title: 야근 지수, Time: 130.02 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.js
@@ -1,29 +1,68 @@
-function solution(n, works) {
-    works.sort((a, b) => b - a);
-    let before = [works[0]];
-    let i = 1;
-    
-    while(n > 0 && works[works.length - 1]) {
-        while(before[before.length - 1] === works[i]) {
-            before.push(works[i]);
-            i += 1;
-        }
-        
-        const gap = works[i]? before[before.length - 1] - works[i] : before[before.length - 1];
-        const length = Math.min(before.length, n);
-        const minus = (gap * length > n)? 1 : gap;
-        for(let j=0; j<length; j++) {
-            before[j] -= minus;
-            works[j] -= minus;
-        }
-        
-        n -= minus * length;
+class MaxHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  push(value) {
+    this.heap.push(value); // 마지막 트리에 추가
+    let currentIndex = this.heap.length - 1;
+    let parentIndex = Math.floor(currentIndex / 2);
+
+    while (parentIndex !== 0 && this.heap[parentIndex] < value) {
+      const temp = this.heap[parentIndex];
+      this.heap[parentIndex] = value;
+      this.heap[currentIndex] = temp;
+
+      currentIndex = parentIndex;
+      parentIndex = Math.floor(currentIndex / 2);
     }
-    
-    return works.reduce((sum, work) => {
-        if(work > 0) {
-            sum += Math.pow(work, 2);
-        }
-        return sum;
-    }, 0);
+  }
+
+  pop() {
+    if (this.heap.length === 2) return this.heap.pop(); // 루트 정점만 남은 경우
+
+    const returnValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+    while (
+      this.heap[currentIndex] < this.heap[leftIndex] ||
+      this.heap[currentIndex] < this.heap[rightIndex]
+    ) {
+      if (this.heap[leftIndex] < this.heap[rightIndex]) {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[rightIndex];
+        this.heap[rightIndex] = temp;
+        currentIndex = rightIndex;
+      } else {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[leftIndex];
+        this.heap[leftIndex] = temp;
+        currentIndex = leftIndex;
+      }
+      leftIndex = currentIndex * 2;
+      rightIndex = currentIndex * 2 + 1;
+    }
+    return returnValue;
+  }
+}
+
+function solution(n, works) {
+  const heap = new MaxHeap();
+
+  for (let i = 0; i < works.length; i++) {
+    heap.push(works[i]);
+  }
+
+  for (let i = 0; i < n; i++) {
+    let peekValue = heap.pop() - 1;
+    if (peekValue < 0) peekValue = 0;
+    heap.push(peekValue);
+  }
+
+  return heap.heap
+    .filter((item) => item !== null)
+    .reduce((acc, item) => acc + item ** 2, 0);
 }
