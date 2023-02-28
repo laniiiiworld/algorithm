e5fa7b0a92757bbbb17a1847597bbfae2ca71ece
Lani <laniiiiworld@gmail.com>
2023-02-28 16:20:45 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.21 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,33 +1,19 @@
 function solution(s) {
-    const left = ['[', '{', '('];
-    const right = [']', '}', ')'];
-    const isCorrect = (str) => {
-        let sum = 0;
-        const stack = [];
-        for(let i=0; i<str.length; i++) {
-            const now = str.charAt(i);
-            if(left.includes(now)) {
-                sum += 1;
-                stack.push(left.indexOf(now));
-            } else {
-                sum -= 1;
-                if(sum < 0) return false;
-                const nowIndex = right.indexOf(now);
-                if(stack[stack.length - 1] - nowIndex === 0) {
-                    stack.pop();
-                } else {
-                    stack.push(-nowIndex);
-                }
-            }
-        }
-        return stack.length === 0;
-    };
-    
     let answer = 0;
-    for(let i=0; i<s.length; i++) {
-        if(!isCorrect(s.slice(i) + s.slice(0, i))) continue;
-        
-        answer += 1;
+
+    for (let i = 0; i < s.length; i++) {
+        s = s.slice(1, s.length)+s[0];
+
+        let string = s;
+
+        for (let j = 0; j < Math.floor(s.length/2); j++) {
+            string = string.replace(/([\[][\]]|[\{][\}]|[\(][\)])+/g, "");
+
+            if (!string) break;
+        }
+
+        answer += !string ? 1 : 0;
     }
+
     return answer;
 }
