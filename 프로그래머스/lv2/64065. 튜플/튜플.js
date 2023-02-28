c5443f71e7b189e2ff78b7731ccd948e1143f2b5
Lani <laniiiiworld@gmail.com>
2023-02-28 11:33:53 +0900

    [level 2] Title: 튜플, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -1,8 +1,15 @@
 function solution(s) {
-    return JSON.parse(s.replace(/{/g,'[').replace(/}/g,']'))
-               .sort((a,b) => a.length - b.length)
-               .reduce((arr, item) => {
-                   const str = item.filter( v => !arr.includes(Number(v)) );
-                   return arr = [...arr, Number(str)];
-               }, []);
+    const answer = new Set();
+    const arr = s.replaceAll('{{', '')
+                 .split('},{')
+                 .map(value => value.split(','))
+                 .sort((a, b) => a.length - b.length);
+    
+    for(const item of arr) {
+        for(const value of item) {
+            answer.add(parseInt(value));
+        }
+    }
+    
+    return [...answer];
 }
