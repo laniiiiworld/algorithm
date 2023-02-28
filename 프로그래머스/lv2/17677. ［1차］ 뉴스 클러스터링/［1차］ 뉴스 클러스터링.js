a6c8b9b4f09c4cf609009b5dfc2373606076136e
Lani <laniiiiworld@gmail.com>
2023-02-28 14:26:31 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,47 +1,26 @@
-/**
- * 문자열 str에 대한 다중 집합을 구하는 함수
- */
-function getMultipleSets(str) {
-    const arr = [];
-    for(let i=0; i<str.length-1; i++) {
-        const item = `${str[i]}${str[i+1]}`;
-        if(/[^a-zA-Z]/g.test(item)) continue;
-        arr.push(item.toUpperCase());
-    }
-    return arr;
-}
-
-/**
- *  두 배열의 교집합 개수를 구하는 함수
- */
-function getIntersectionCount(longArr, sortArr) {
-    let count = 0;
-    for(let i=0; i<longArr.length; i++) {
-        if(sortArr.length === 0) break;
-        const index = sortArr.indexOf(longArr[i]);
-        if(index === -1) continue;
-        sortArr.splice(index, 1);
-        count++;
-    }
-    return count;
-}
-
 function solution(str1, str2) {
+    const getMultipleSets = (str) => {
+        const arr = [];
+        const strs = new Map();
+        
+        for(let i=1; i<str.length; i++) {
+            const s = str.substr(i-1, 2).toLowerCase();
+            if(s.length > s.replace(/[^a-z]/g, '').length) continue;
+            const count = strs.get(s) + 1 || 1;
+            strs.set(s, count);
+            arr.push(`${s}${count}`);
+        }
+        
+        return arr.sort();
+    };
+    
     const arr1 = getMultipleSets(str1);
     const arr2 = getMultipleSets(str2);
     
-    let longArr;
-    let sortArr;
-    if(Math.max(arr1.length, arr2.length) === arr1.length) {
-        longArr = arr1;
-        sortArr = arr2;
-    } else {
-        longArr = arr2;
-        sortArr = arr1;
-    }
+    if(arr1.length === 0 && arr2.length === 0) return 65536;
     
-    const intersection = getIntersectionCount(longArr, sortArr);
-    const union = longArr.length + sortArr.length;
-    const jaccard = intersection || union? intersection/union : 1;
-    return Math.floor(jaccard * 65536);
+    const commonLength = arr1.length + arr2.length - new Set([...arr1, ...arr2]).size;
+    const allLength = arr1.length + arr2.length - commonLength;
+    
+    return Math.floor(commonLength / allLength * 65536);
 }
