052c416931316c1bdfa8eb608334bea4c2b821ed
Lani <laniiiiworld@gmail.com>
2023-02-28 16:25:34 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,19 +1,38 @@
-function solution(s) {
-    let answer = 0;
+//올바른 괄호 여부 확인 함수
+function isCorrect(str) {
+    const all = ['(','[','{',')',']','}'];
+    const left = ['(','[','{'];
+    const sum = [0,0,0];
+    let stack = []; //열어주는 괄호들의 index를 저장하는 배열
 
-    for (let i = 0; i < s.length; i++) {
-        s = s.slice(1, s.length)+s[0];
+    for(let i=0; i<str.length; i++) {
+        const index = all.indexOf(str[i]) % 3;
 
-        let string = s;
-
-        for (let j = 0; j < Math.floor(s.length/2); j++) {
-            string = string.replace(/([\[][\]]|[\{][\}]|[\(][\)])+/g, "");
-
-            if (!string) break;
+        if(left.indexOf(str[i]) > -1) {
+            sum[index] += 1;
+            stack.push(index);
+        } else {
+            //괄호를 열기 전에 닫으려 하는 경우 X
+            if(stack.length === 0) return 0;
+            //열어주는 괄호와 짝이 맞지 않은 경우 X
+            if(index !== stack[stack.length-1]) return 0;
+            sum[index] -= 1;
+            stack.pop();
         }
-
-        answer += !string ? 1 : 0;
     }
 
-    return answer;
+    //열어주는 괄호들이 모두 닫히지 않은 경우 X
+    return sum.filter(value => value !== 0).length? 0 : 1;
+}
+
+function solution(s) {
+    const sLength = s.length;
+    let count =0;
+    s += s;
+
+    for(let start=0; start<sLength; start++) {
+        count += isCorrect(s.slice(start, start + sLength));
+    }
+
+    return count;
 }
