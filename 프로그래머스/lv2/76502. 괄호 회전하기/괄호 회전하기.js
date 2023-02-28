847aff0c606c1f2c9f6fdad98593bd5551ddf56f
Lani <laniiiiworld@gmail.com>
2023-02-28 16:16:31 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,40 +1,32 @@
-//올바른 괄호 여부 확인 함수
-function isCorrect(str) {
-    const all = ['(','[','{',')',']','}'];
-    const left = ['(','[','{'];
-    const sum = [0,0,0];
-    let stack = []; //열어주는 괄호들의 index를 저장하는 배열
-
-    for(let i=0; i<str.length; i++) {
-        const index = all.indexOf(str[i]) % 3;
-
-        if(left.indexOf(str[i]) > -1) {
-            sum[index] += 1;
-            stack.push(index);
-        } else {
-            //괄호를 열기 전에 닫으려 하는 경우 X
-            if(stack.length === 0) return 0;
-            //열어주는 괄호와 짝이 맞지 않은 경우 X
-            if(index !== stack[stack.length-1]) return 0;
-            sum[index] -= 1;
-            stack.pop();
-        }
-    }
-
-    //열어주는 괄호들이 모두 닫히지 않은 경우 X
-    return sum.filter(value => value !== 0).length? 0 : 1;
-}
-
 function solution(s) {
-    if(s.length % 2 === 1) return 0;
+    const left = ['[', '{', '('];
+    const right = [']', '}', ')'];
+    const isCorrect = (str) => {
+        let sum = 0;
+        const stack = [];
+        for(let i=0; i<str.length; i++) {
+            const now = str.charAt(i);
+            if(left.includes(now)) {
+                sum += 1;
+                stack.push(left.indexOf(now));
+            } else {
+                sum -= 1;
+                if(sum < 0) return false;
+                const nowIndex = right.indexOf(now);
+                if(stack[stack.length - 1] - nowIndex === 0) {
+                    stack.pop();
+                } else {
+                    stack.push(-nowIndex);
+                }
+            }
+        }
+        return sum === 0 && stack.length === 0;
+    };
     
-    const sLength = s.length;
-    let count =0;
-    s += s;
-
-    for(let start=0; start<sLength; start++) {
-        count += isCorrect(s.slice(start, start + sLength));
+    let answer = 0;
+    for(let i=0; i<s.length; i++) {
+        const str = s.slice(i) + s.slice(0, i);
+        if(isCorrect(str)) answer += 1;
     }
-
-    return count;
+    return answer;
 }
