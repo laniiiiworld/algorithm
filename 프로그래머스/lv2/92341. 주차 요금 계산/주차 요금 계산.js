1a6551a827123b946c2d09681873f987e828d82e
Lani <laniiiiworld@gmail.com>
2023-03-02 13:24:02 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,43 +1,36 @@
 function solution(fees, records) {
     const answer = [];
-    const [baseTime, basePrice, unitTime, unitPrice] = fees;
-    const carNumbers = []; //계산된 차량 번호들을 저장하는 배열
-    
-    records = records.map(record => record.split(' '));
-    
-    //시각(시간:분)을 입력받아 분으로 변환해주는 함수
-    const getMinute = (time) => {
-        const [hour, minute] = time.split(':');
-        return Number(hour)*60 + Number(minute);
+    const cars = new Map();
+    const [baseTime, baseFee, unitTimeMi, unitFee] = fees;
+    const getTotalFee = (mi) => {
+        const time = mi - baseTime;
+        if(time <= 0) return baseFee;
+        return baseFee + Math.ceil(time/unitTimeMi) * unitFee;
     };
-    //마지막 시각. 출차되지 않은 경우 계산에 필요
-    const LAST_TIME = getMinute('23:59');
     
-    for(let i=0; i<records.length; i++) {
-        const [,car,] = records[i];
-        if(carNumbers.includes(car)) continue;
-        carNumbers.push(car);
-        
-        const list = records.filter(record => record[1] === car);
-        //누적 주차시간(분)
-        const totalTime = list.reduce((totalTime, item) => {
-            const [time, , inout] = item;
-            if(inout === 'IN') {
-                totalTime += (LAST_TIME - getMinute(time));
-            } else {
-                totalTime -= (LAST_TIME - getMinute(time));
-            }
-            return totalTime;
-        }, 0);
-        //청구할 주차요금
-        let totalFee = basePrice;
-        if(totalTime > baseTime) {
-            totalFee += Math.ceil((totalTime - baseTime)/unitTime) * unitPrice;
-        }
-        
-        answer.push({car, totalFee});
+    for(const info of records) {
+        const [time, number, inout] = info.split(' ');
+        const infos = cars.get(number) || [];
+        cars.set(number, [...infos, info]);
     }
     
-    return answer.sort((a, b) => a.car - b.car)
-                 .map(item => item.totalFee);
+    for(const number of [...cars.keys()].sort((a, b) => a - b)) {
+        const infos = cars.get(number);
+        if(infos.length % 2) infos.push(`23:59 ${number} OUT`);
+        let totalTimes = [0, 0];
+        
+        for(const info of infos) {
+            const [time, number, inout] = info.split(' ');
+            let [hh, mi] = time.split(':').map(value => parseInt(value));
+            hh = (hh + Math.floor(mi / 60));
+            mi = mi % 60;
+            
+            totalTimes[0] += (inout === 'IN')? -hh : hh;
+            totalTimes[1] += (inout === 'IN')? -mi : mi;
+        }
+        
+        answer.push(getTotalFee(totalTimes[0]*60 + totalTimes[1]));
+    }
+    
+    return answer;
 }
