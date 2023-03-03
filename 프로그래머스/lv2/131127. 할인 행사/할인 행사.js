9be4b9861b220bf0d97c2a16f6b49fd3c990de9d
Lani <laniiiiworld@gmail.com>
2023-03-03 14:39:50 +0900

    [level 2] Title: 할인 행사, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -1,39 +1,39 @@
-//정현이가 원하는 제품과 수량이 할인하는 날짜와 10일 연속으로 일치하는지 확인
-function compareShoppingList(want, number, nowDiscounts) {
-    let index = 0;
-    while(index < want.length) {
-        const count = nowDiscounts.get(want[index]) || 0;
-        if(count < number[index]) return 0;
-        index++;
-    }
-    return 1;
-}
-
 function solution(want, number, discount) {
     let answer = 0;
-    let nowDiscounts = new Map(); //10일 동안 할인하는 상품들
-    let index = 0;
-    let count = 0;
+    const list = new Map();
+    const isEqual = () => {
+        if(list.size !== want.length) return false;
+        
+        for(let i=0; i<want.length; i++) {
+            const count = list.get(want[i]) || 0;
+            if(count !== number[i]) return false;
+        }
+        
+        return true;
+    };
     
-    //1일차 확인
-    while(index < 10) {
-        count = nowDiscounts.get(discount[index]) || 0;
-        nowDiscounts.set(discount[index], count + 1);
-        index++;
+    for(let i=0; i<10; i++) {
+        const item = discount[i];
+        const count = list.get(item) || 0;
+        list.set(item, count + 1);
     }
     
-    answer += compareShoppingList(want, number, nowDiscounts);
+    if(isEqual()) answer += 1;
     
-    //2일차부터 확인
-    for(index=1; index<=discount.length-10; index++) {
-        //전날 할인상품 수량 - 1
-        count = nowDiscounts.get(discount[index-1]);
-        nowDiscounts.set(discount[index-1], count - 1);
-        //10일차 할인상품 수량 + 1
-        count = nowDiscounts.get(discount[index+9]) || 0;
-        nowDiscounts.set(discount[index+9], count + 1);
+    for(let i=10; i<discount.length; i++) {
+        const minusItem = discount[i-10];
+        const minusCount = list.get(minusItem);
+        if(minusCount === 1) {
+            list.delete(minusItem);
+        } else {
+            list.set(minusItem, minusCount - 1);
+        }
         
-        answer += compareShoppingList(want, number, nowDiscounts);
+        const plusItem = discount[i];
+        const plusCount = list.get(plusItem) || 0;
+        list.set(plusItem, plusCount + 1);
+        
+        if(isEqual()) answer += 1;
     }
     
     return answer;
