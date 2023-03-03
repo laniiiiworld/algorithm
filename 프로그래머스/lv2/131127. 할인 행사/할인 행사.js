72413c0ccc416ba7dbc484d5fc681e323a14235b
Lani <laniiiiworld@gmail.com>
2023-03-03 14:42:10 +0900

    [level 2] Title: 할인 행사, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -1,40 +1,16 @@
 function solution(want, number, discount) {
-    let answer = 0;
-    const list = new Map();
-    const isEqual = () => {
-        if(list.size !== want.length) return false;
-        
-        for(let i=0; i<want.length; i++) {
-            const count = list.get(want[i]) || 0;
-            if(count !== number[i]) return false;
+    let count = 0;
+    for (let i = 0; i < discount.length - 9; i++) {
+        const slice = discount.slice(i, i+10);
+
+        let flag = true;
+        for (let j = 0; j < want.length; j++) {
+            if (slice.filter(item => item === want[j]).length !== number[j]) {
+                flag = false;
+                break;
+            }
         }
-        
-        return true;
-    };
-    
-    for(let i=0; i<10; i++) {
-        const item = discount[i];
-        const count = list.get(item) || 0;
-        list.set(item, count + 1);
+        if (flag) count += 1;
     }
-    
-    if(isEqual()) answer += 1;
-    
-    for(let i=10; i<discount.length; i++) {
-        const minusItem = discount[i-10];
-        const minusCount = list.get(minusItem);
-        if(minusCount === 1) {
-            list.delete(minusItem);
-        } else {
-            list.set(minusItem, minusCount - 1);
-        }
-        
-        const plusItem = discount[i];
-        const plusCount = list.get(plusItem) || 0;
-        list.set(plusItem, plusCount + 1);
-        
-        if(isEqual()) answer += 1;
-    }
-    
-    return answer;
+    return count;
 }
