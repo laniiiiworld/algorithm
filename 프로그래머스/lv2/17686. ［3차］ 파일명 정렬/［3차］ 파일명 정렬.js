c3d057f94ed4549c037df3fb90bb141bcc512ad9
Lani <laniiiiworld@gmail.com>
2023-03-04 08:36:24 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 1.29 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,12 +1,22 @@
 function solution(files) {
-    return files.map(file => {
-                    const [fileName, head, number, tail] = file.match(/([\D]+)([0-9]+)(.*)$/);
-                    return [fileName, head.toLowerCase(), number, tail];
-                })
-                .sort((a, b) => {
-                    if (a[1] > b[1]) return 1;
-                    if (a[1] < b[1]) return -1;
-                    return a[2] - b[2];
-                })
-                .map(item => item[0]);
+    return files
+        .map((file, i) => {
+            const matched = file.match(/^([a-zA-Z-\. ]+)([0-9]+)(.*)$/);
+            return {
+                name: file,
+                head: matched[1].toLowerCase(),
+                number: parseInt(matched[2]),
+                index: i
+            };
+        })
+        .sort((file1, file2) => {
+            if (file1.head !== file2.head) {
+                return (file1.head > file2.head) ? 1 : -1;
+            }
+            if (file1.number !== file2.number) {
+                return file1.number - file2.number;
+            }
+            return file1.index - file2.index;
+        })
+        .map(file => file.name);
 }
