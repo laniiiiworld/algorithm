efb71eccddfd91601054609fa3a7bd3be2c4e05b
Lani <laniiiiworld@gmail.com>
2023-03-07 19:28:32 +0900

    [level 2] Title: 가장 큰 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,6 @@
 function solution(numbers) {
     const answer = numbers
-                    .sort((a, b) => parseInt(`${b}${a}`) - parseInt(`${a}${b}`))
+                    .sort((a, b) => `${b}${a}` - `${a}${b}`)
                     .join('');
     return answer[0] === '0' ? '0' : answer;
 }
