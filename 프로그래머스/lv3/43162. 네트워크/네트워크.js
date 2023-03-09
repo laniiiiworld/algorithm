e2e2afbce9def41579e09523bdeffe957545bf0d
Lani <laniiiiworld@gmail.com>
2023-03-09 10:06:20 +0900

    [level 3] Title: 네트워크, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,32 +1,30 @@
+//dfs
 function solution(n, computers) {
-    let answer = 1; //network 개수
-    const links = new Map(); //key와 연결된 컴퓨터들을 저장한 정보
-    const visited = Array(n).fill(false); //index번째 컴퓨터 방문여부를 저장한 정보
+    let count = 0;
+    const graph = Array.from({length: n}, () => []);
+    const visited = Array(n).fill(false);
+    const getNewNetworks = (computer) => {
+        visited[computer] = true;
+        const connectedComputers = graph[computer];
+        while(connectedComputers.length) {
+            const next = connectedComputers.pop();
+            getNewNetworks(next);
+        }
+    };
     
-    for(let i=0; i<computers.length; i++) {
-        for(let j=0; j<computers[0].length; j++) {
-            if(computers[i][j] === 0 || i === j) continue;
-            if(!links.has(i)) links.set(i, []);
-            links.set(i, [...links.get(i), j]);
+    for(let i=0; i<n; i++) {
+        for(let j=0; j<n; j++) {
+            if(i === j || !computers[i][j]) continue;
+            if(!graph[i].includes(j)) graph[i].push(j);
+            if(!graph[j].includes(i)) graph[j].push(i);
         }
     }
     
-    const queue = [0];
-    while(visited.includes(false)) {
-        let current = null;
-        if(queue.length) {
-            current = queue.shift();
-        } else {
-            answer += 1;
-            current = visited.findIndex(value => value === false);
-        }
-        visited[current] = true;
-        const nodes = links.get(current) || [];
-        for(const node of nodes) {
-            if(visited[node]) continue;
-            queue.push(node);
-        }
+    for(let i=0; i<n; i++) {
+        if(visited[i]) continue;
+        count += 1;
+        getNewNetworks(i);
     }
     
-    return answer;
+    return count;
 }
