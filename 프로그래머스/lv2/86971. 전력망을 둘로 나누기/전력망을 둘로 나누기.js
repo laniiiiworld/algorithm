d56e056a0cbe1be54244d937b689cfb6e27f0d6e
Lani <laniiiiworld@gmail.com>
2023-03-12 16:48:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.98 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,33 +1,31 @@
 function solution(n, wires) {
-    let answer = Infinity;
-    const makeNewNetwork = (wires) => {
-        let tower = 0;
+    let answer = n;
+    const getNewNetwork = (graph, start, visited) => {
+        const queue = [start];
+        visited[start] = true;
         
-        const visited = Array(n+1).fill(false);
-        const gragh = Array.from({length:n+1}, () => []);
-        for(const [start, end] of wires) {
-            gragh[start].push(end);
-            gragh[end].push(start);
-        }
-        
-        const queue = [wires[0][0]];
         while(queue.length) {
-            const current = queue.shift();
-            visited[current] = true;
-            tower += 1;
-            for(const node of gragh[current]) {
-                if(visited[node]) continue;
-                queue.push(node);
+            const value = queue.shift();
+            for(const i of graph[value]) {
+                if(visited[i]) continue;
+                queue.push(i);
+                visited[i] = true;
             }
         }
         
-        return tower;
+        return visited.filter(value => value === true).length;
     };
     
     for(let i=0; i<wires.length; i++) {
-        const newWires = [...wires.slice(0, i), ...wires.slice(i+1)];
-        const count = makeNewNetwork(newWires);
-        answer = Math.min(answer, Math.abs(n - count * 2));
+        const graph = Array.from({length: n+1}, () => []);
+        const visited = Array(n+1).fill(false);
+        const copyWires = [...wires.slice(0, i), ...wires.slice(i+1)];
+        for(const [start, end] of copyWires) {
+            graph[start].push(end);
+            graph[end].push(start);
+        }
+        const count = getNewNetwork(graph, copyWires[0][0], visited);
+        answer = Math.min(answer, Math.abs(count - (n - count)));
     }
     
     return answer;
