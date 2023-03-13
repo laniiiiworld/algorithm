69dcfbbcc591323cd027d98d1ef751e11c3bba12
Lani <laniiiiworld@gmail.com>
2023-03-13 19:11:54 +0900

    [level 2] Title: 타겟 넘버, Time: 1.10 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/README.md
+++ 프로그래머스/lv2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 35.5 MB, 시간: 1.12 ms
+메모리: 35.6 MB, 시간: 1.10 ms
 
 ### 구분
 
--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,16 +1,15 @@
 function solution(numbers, target) {
     let answer = 0;
-    const makeNumber = (sum, index) => {
+    const dfs = (sum, index) => {
         if(index === numbers.length) {
             answer += (sum === target)? 1 : 0;
             return;
         }
-        
-        makeNumber(sum + numbers[index], index + 1);
-        makeNumber(sum - numbers[index], index + 1);
+        dfs(sum + numbers[index], index + 1);
+        dfs(sum - numbers[index], index + 1);
     };
     
-    makeNumber(0, 0);
+    dfs(0, 0);
     
     return answer;
 }
