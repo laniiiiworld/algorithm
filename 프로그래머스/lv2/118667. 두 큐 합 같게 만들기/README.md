be3602e07d1124a131e3b18d99317b9541a61f41
Lani <laniiiiworld@gmail.com>
2023-03-18 18:49:34 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 40.69 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
 
 ### 성능 요약
 
-메모리: 47.5 MB, 시간: 17.57 ms
+메모리: 52.4 MB, 시간: 40.69 ms
 
 ### 구분
 
--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,28 +1,51 @@
-function solution(queue1, queue2) {
-    const maxIndex = queue1.length * 2 - 1;
-    let count = 0;
-    let sum1 = queue1.reduce((sum, num) => sum+num, 0);
-    let sum2 = queue2.reduce((sum, num) => sum+num, 0);
-    let start1 = 0;
-    let start2 = 0;
-    let firstEl = 0;
-
-    while(sum1 !== sum2) {
-        if(start1 >= maxIndex && start2 >= maxIndex) return -1;
-        count++;
-
-        if(sum1 > sum2) {
-            firstEl = queue1[start1++];
-            queue2.push(firstEl);
-            sum1 -= firstEl;
-            sum2 += firstEl;
-        } else {
-            firstEl = queue2[start2++];
-            queue1.push(firstEl);
-            sum1 += firstEl;
-            sum2 -= firstEl;
-        }
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.front = 0;
+        this.rear = -1;
     }
-
-    return count;
+    enqueue(value) {
+        this.queue.push(value);
+        this.rear += 1;
+    }
+    dequeue() {
+        const returnValue = this.queue[this.front];
+        delete this.queue[this.front++];
+        return returnValue;
+    }
+}
+function solution(queue1, queue2) {
+    const arr1 = new Queue();
+    const arr2 = new Queue();
+    let sum1 = 0;
+    let sum2 = 0;
+    for(const v of queue1) {
+        arr1.enqueue(v);
+        sum1 += v;
+    }
+    for(const v of queue2) {
+        arr2.enqueue(v);
+        sum2 += v;
+    }
+    
+    let count = 0;
+    while(count < (queue1.length * 3)) {
+        if(sum1 === sum2) return count;
+        
+        if(sum1 < sum2) {
+            const value = arr2.dequeue();
+            arr1.enqueue(value);
+            sum1 += value;
+            sum2 -= value;
+        } else {
+            const value = arr1.dequeue();
+            arr2.enqueue(value);
+            sum1 -= value;
+            sum2 += value;
+        }
+        
+        count += 1;
+    }
+    
+    return -1;
 }
