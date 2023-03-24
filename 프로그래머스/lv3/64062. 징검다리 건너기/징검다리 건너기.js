0ac5fdeb73dd9b3328674741f4df4ee1667a2cc3
Lani <laniiiiworld@gmail.com>
2023-03-25 06:49:37 +0900

    [level 3] Title: 징검다리 건너기, Time: 20.27 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,20 +1,26 @@
 function solution(stones, k) {
-    let left = 1;
-    let right = 200000000;
+    const binarySearch = (start, end) => {
+        while (start <= end) {
+            const middle = parseInt((start + end) / 2);
+            
+            let count = 0;
+            for (const stone of stones) {
+                if (stone <= middle) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+                if (count >= k) break;
+            }
 
-    while(left <= right) {
-        const mid = (left + right) / 2 >> 0;
-
-        let count = 0;
-        for(let i = 0; i < stones.length; i++) {
-            if(count === k) break;
-            if(stones[i] - mid <= 0) count++;
-            else count = 0;
+            if (count >= k) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
         }
-        
-        if(count === k) right = mid - 1;
-        else left = mid + 1;
-    }
-
-    return left;
+        return start;
+    };
+    
+    return binarySearch(0, 200000000);
 }
