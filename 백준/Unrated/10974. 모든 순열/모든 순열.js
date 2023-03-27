1bde91f6f7b1813f65019a93265947f8839584b3
Lani <laniiiiworld@gmail.com>
2023-03-27 19:24:19 +0900

    [Unrated] Title: 모든 순열, Time: 264 ms, Memory: 22256 KB -BaekjoonHub

---
+++ 백준/Unrated/10974. 모든 순열/모든 순열.js
@@ -0,0 +1,22 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const n = Number(input[0]);
+const m = n;
+const answer = [];
+const visited = Array(n+1).fill(false);
+
+function dfs(arr) {
+    if(arr.length === m) {
+        answer.push(arr.join(' '));
+        return;
+    }
+    for(let i=1; i<=n; i++) {
+        if(visited[i]) continue;
+        visited[i] = true;
+        dfs([...arr, i]);
+        visited[i] = false;
+    }
+}
+
+dfs([]);
+
+console.log(answer.join('\n'));
