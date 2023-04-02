d108ded840db976dc69ec8d99144933324de7925
Lani <laniiiiworld@gmail.com>
2023-04-02 12:03:12 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 252 ms, Memory: 18820 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.js
@@ -0,0 +1,34 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const [n, m] = input[0].split(' ').map(Number); //graph의 크기
+let line = 1;
+
+const arr = []; //2차원 배열
+arr.push(new Array(m+1).fill(0));
+for(let i = 0; i < n; i++) {
+    const row = line + i;
+    const lineArr = [0];
+    // a b
+    // c d
+    // => c + b + d - a
+    for(const value of input[row].split(' ').map(Number)) {
+        const col = lineArr.length;
+        lineArr.push(lineArr[col - 1] + arr[row - 1][col] + value - arr[row - 1][col - 1]);
+    }
+    arr.push(lineArr);
+}
+
+line += n;
+
+const k = Number(input[line++]);
+const answer = [];
+for(let index = 0; index < k; index++) {
+    const [i, j, x, y] = input[line + index].split(' ').map(Number);
+    // 1 2 3
+    // 4 5 6
+    // 7 8 9
+    // 5, 6, 8, 9 위치의 합 => 9 - 7 - 3 + 1
+    const result = arr[x][y] - arr[x][j - 1] - arr[i - 1][y] + arr[i - 1][j - 1];
+    answer.push(result);
+}
+
+console.log(answer.join('\n'));
