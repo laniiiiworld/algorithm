deff952f880d2f001f598150da59945bdda468b1
Lani <laniiiiworld@gmail.com>
2023-04-11 09:52:10 +0900

    [Silver II] Title: 병사 배치하기, Time: 188 ms, Memory: 9728 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.js
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.js
@@ -4,10 +4,12 @@ const arr = input[1].split(' ').map(Number);
 const dp = Array(n).fill(1);
 
 for(let i = 1; i < n; i++) {
+    let max = 0;
     for(let j = 0; j < i; j++) {
-        if(arr[j] <= arr[i]) continue;
-        dp[i] = Math.max(dp[j] + 1, dp[i]);
+        if(arr[i] >= arr[j]) continue;
+        max = Math.max(max, dp[j]);
     }
+    dp[i] = max + 1;
 }
 
 console.log(n - Math.max(...dp));
