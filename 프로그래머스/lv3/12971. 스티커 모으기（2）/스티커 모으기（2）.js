d66b9b9dcb05309ca42f1b14b208969c3087145d
Lani <laniiiiworld@gmail.com>
2023-04-11 22:55:14 +0900

    [level 3] Title: 스티커 모으기(2), Time: 25.91 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -1,17 +1,25 @@
 function solution(sticker) {
-    if(sticker.length <= 3) return Math.max(...sticker);
+    const n = sticker.length;
+    if(n === 1) return sticker[0];
+    if(n === 2) return Math.max(sticker[0], sticker[1]);
     
-    const dp1 = [sticker[0], Math.max(sticker[0], sticker[1])];
-    const dp2 = [0, sticker[1], Math.max(sticker[1], sticker[2])];
+    const dp1 = Array(n).fill(0);
+    const dp2 = Array(n).fill(0);
     
-    for(let i=2; i<sticker.length; i++) {
-        if(i !== sticker.length - 1) {
-            dp1.push(Math.max(dp1[i-1], dp1[i-2] + sticker[i]));
+    dp1[0] = sticker[0];
+    dp1[1] = Math.max(sticker[0], sticker[1]);
+    
+    dp2[1] = sticker[1];
+    dp2[2] = Math.max(sticker[1], sticker[2]);
+        
+    for(let i = 2; i < n; i++) {
+        if(i < n - 1) {
+            dp1[i] = Math.max(dp1[i - 2] + sticker[i], dp1[i - 1]);
         }
-        if(i !== 2) {
-            dp2.push(Math.max(dp2[i-1], dp2[i-2] + sticker[i]));
+        if(i > 2) {
+            dp2[i] = Math.max(dp2[i - 2] + sticker[i], dp2[i - 1]);
         }
     }
     
-    return Math.max(dp1[dp1.length - 1], dp2[dp2.length - 1]);
+    return Math.max(dp1[n - 2], dp2[n - 1]);
 }
