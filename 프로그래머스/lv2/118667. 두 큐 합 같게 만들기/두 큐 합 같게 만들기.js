3f7c03b48aec1b38d91a6c11719672109eec7f7e
Lani <laniiiiworld@gmail.com>
2023-04-13 00:33:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 43.09 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -2,36 +2,39 @@ class Queue {
     constructor() {
         this.queue = [];
         this.front = 0;
-        this.rear = -1;
+        this.rear = 0;
     }
     enqueue(value) {
         this.queue.push(value);
         this.rear += 1;
     }
     dequeue() {
-        const returnValue = this.queue[this.front];
+        const value = this.queue[this.front];
         delete this.queue[this.front++];
-        return returnValue;
+        return value;
     }
 }
 function solution(queue1, queue2) {
     const arr1 = new Queue();
     const arr2 = new Queue();
+    
     let sum1 = 0;
-    let sum2 = 0;
-    for(const v of queue1) {
-        arr1.enqueue(v);
-        sum1 += v;
+    for(const value of queue1) {
+        arr1.enqueue(value);
+        sum1 += value;
     }
-    for(const v of queue2) {
-        arr2.enqueue(v);
-        sum2 += v;
+    let sum2 = 0;
+    for(const value of queue2) {
+        arr2.enqueue(value);
+        sum2 += value;
     }
     
-    let count = 0;
-    while(count < (queue1.length * 3)) {
-        if(sum1 === sum2) return count;
-        
+    let answer = 0;
+    let count = queue1.length * 4;
+    while(count--) {
+        if(sum1 === sum2) {
+            return answer;
+        }
         if(sum1 < sum2) {
             const value = arr2.dequeue();
             arr1.enqueue(value);
@@ -43,8 +46,7 @@ function solution(queue1, queue2) {
             sum1 -= value;
             sum2 += value;
         }
-        
-        count += 1;
+        answer += 1;
     }
     
     return -1;
