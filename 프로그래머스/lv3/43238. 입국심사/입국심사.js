68648948c12c571c62e1ae3e88b9cda60474a17f
Lani <laniiiiworld@gmail.com>
2023-04-12 18:02:51 +0900

    [level 3] Title: 입국심사, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.js
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.js
@@ -4,12 +4,13 @@ function solution(n, times) {
     
     while(left <= right) {
         const mid = Math.floor((left + right) / 2);
-        const sum = times.reduce((sum, time) => sum += Math.floor(mid/time), 0);
+        //각 심사관이 mid 시간동안 심사할 수 있는 사람 수를 더한 값
+        const count = times.reduce((acc, cur) => acc += Math.floor(mid / cur), 0);
         
-        if(sum < n) {
-            left = mid + 1;
-        } else {
+        if(count >= n) {
             right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
     
