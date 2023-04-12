ebb990bda572b72fa6d87f7e3d2315f890f31cd0
Lani <laniiiiworld@gmail.com>
2023-04-12 18:22:33 +0900

    [level 3] Title: 징검다리 건너기, Time: 30.64 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/lv3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062) 
 
 ### 성능 요약
 
-메모리: 45.1 MB, 시간: 20.27 ms
+메모리: 45 MB, 시간: 30.64 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 64.1<br/>효율성: 35.9<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,26 +1,26 @@
 function solution(stones, k) {
-    const binarySearch = (start, end) => {
-        while (start <= end) {
-            const middle = parseInt((start + end) / 2);
-            
-            let count = 0;
-            for (const stone of stones) {
-                if (stone <= middle) {
-                    count++;
-                } else {
-                    count = 0;
-                }
-                if (count >= k) break;
-            }
-
-            if (count >= k) {
-                end = middle - 1;
+    let left = 0;
+    let right = 200_000_000;
+    
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        let maxCount = 0;
+        let count = 0;
+        for(const stone of stones) {
+            if(stone <= mid) {
+                count += 1;
+                maxCount = Math.max(maxCount, count);
             } else {
-                start = middle + 1;
+                count = 0;
             }
         }
-        return start;
-    };
+        
+        if(maxCount >= k) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
     
-    return binarySearch(0, 200000000);
+    return left;
 }
