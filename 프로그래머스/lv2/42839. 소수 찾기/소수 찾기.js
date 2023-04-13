db04e5f6690a400cf87a25cba0e7d446cc0041fd
Lani <laniiiiworld@gmail.com>
2023-04-13 17:16:25 +0900

    [level 2] Title: 소수 찾기, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,25 +1,26 @@
 function solution(numbers) {
     const answer = new Set();
     const visited = Array(numbers.length).fill(false);
-    const isPrime = (number) => {
+    const isFrime = (number) => {
         if(number < 2) return false;
-        for(let i=2; i*i<=number; i++) {
+        for(let i = 2; i * i <= number; i++) {
             if(number % i === 0) return false;
         }
         return true;
     };
-    const dfs = (fixed) => {
-        answer.add(Number(fixed));
+    const dfs = (makedNumber) => {
+        if(isFrime(Number(makedNumber))) answer.add(Number(makedNumber));
+        if(makedNumber.length === numbers.length) return;
         
-        for(let i=0; i<numbers.length; i++) {
+        for(let i = 0; i < numbers.length; i++) {
             if(visited[i]) continue;
             visited[i] = true;
-            dfs(`${fixed}${numbers[i]}`);
+            dfs(`${makedNumber}${numbers[i]}`);
             visited[i] = false;
         }
     };
     
-    dfs('0');
+    dfs('');
     
-    return [...answer].filter(v => isPrime(v)).length;
+    return answer.size;
 }
