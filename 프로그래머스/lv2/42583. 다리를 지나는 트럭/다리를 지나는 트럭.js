279ad944109fed0562c69e5c8a7c38959696ca4c
Lani <laniiiiworld@gmail.com>
2023-04-13 11:01:22 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,51 +1,24 @@
-class Queue {
-    constructor() {
-        this.queue = [];
-        this.front = 0;
-        this.rear = 0;
-        this.total = 0;
-    }
-    enqueue(value) {
-        this.queue[this.rear++] = value;
-        this.total += value;
-    }
-    dequeue() {
-        const value = this.queue[this.front];
-        delete this.queue[this.front];
-        this.front += 1;
-        this.total -= value;
-        return value;
-    }
-    peek() {
-        return this.queue[this.front];
-    }
-    size() {
-        return this.rear - this.front;
-    }
-    getTrucksWeight() {
-        return this.total;
-    }
-}
-
 function solution(bridge_length, weight, truck_weights) {
-    let seconds = 0;
-    const times = new Array(truck_weights.length).fill(bridge_length);
-    const moveTrucks = (start, end) => {
-        for(let i=start; i<=end; i++) {
-            times[i] -= 1;
-        }
-    };
-    const bridge = new Queue();
-    let i=0;
+    let answer = 0;
+    const bridge = [];
+    let nextIndex = 0;
+    let nowWeight = 0;
     
-    while(i<truck_weights.length) {
-        seconds += 1;
-        moveTrucks(i - bridge.size(), i-1);
-        if(times[i - bridge.size()] === 0) bridge.dequeue();
-        if(bridge.size() === bridge_length) continue;
-        if(bridge.getTrucksWeight() + truck_weights[i] > weight) continue;
-        bridge.enqueue(truck_weights[i++]);
+    while(nextIndex < truck_weights.length) {
+        answer += 1;
+        bridge.forEach(car => car.time -= 1);
+        while(bridge.length && bridge[0].time === 0) {
+            const truck = bridge.shift();
+            nowWeight -= truck.weight;
+        }
+        
+        if(bridge.length === bridge_length) continue;
+        if(nowWeight + truck_weights[nextIndex] > weight) continue;
+        
+        const truck = {weight: truck_weights[nextIndex++], time: bridge_length};
+        bridge.push(truck);
+        nowWeight += truck.weight;
     }
     
-    return seconds + bridge_length;
+    return answer + bridge[bridge.length - 1].time;
 }
