c957f404e5ae082aa42bb75b1e081a244bf76b80
Lani <laniiiiworld@gmail.com>
2023-04-13 15:09:13 +0900

    [level 2] Title: N-Queen, Time: 530.19 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.js
@@ -1,21 +1,20 @@
 function solution(n) {
-    let count = 0;
+    let answer = 0;
     const queens = [];
-    const dfs = (row) => {
-        if(row === n) {
-            count += 1;
+    const dfs = (row, col) => {
+        if(row === n - 1) {
+            answer += 1;
             return;
         }
-        
         for(let col = 0; col < n; col++) {
-            if(queens.findIndex(([y, x]) => y === row || x === col || row - y === Math.abs(col - x)) > -1) continue;
-            queens.push([row, col]);
-            dfs(row + 1);
+            if(queens.findIndex(([y, x]) => x === col || Math.abs(x - col) === row - y + 1) > -1) continue;
+            queens.push([row + 1, col]);
+            dfs(row + 1, col);
             queens.pop();
         }
     };
     
-    dfs(0);
+    dfs(-1, 0);
     
-    return count;
+    return answer;
 }
