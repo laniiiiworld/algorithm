397ff393498a0a0b8de01a782a8198d2dc5f1bc5
Lani <laniiiiworld@gmail.com>
2023-04-13 23:10:36 +0900

    [Silver I] Title: 숨바꼭질, Time: 500 ms, Memory: 37128 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -0,0 +1,43 @@
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.front = 0;
+        this.rear = 0;
+    }
+    size() {
+        return this.rear - this.front;
+    }
+    enqueue(value) {
+        this.queue.push(value);
+        this.rear += 1;
+    }
+    dequeue() {
+        const value = this.queue[this.front];
+        delete this.queue[this.front++];
+        return value;
+    }
+}
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+
+let answer = Math.abs(n - k);
+const visited = new Set();
+const queue = new Queue();
+queue.enqueue([n, 0]);
+
+while(queue.size()) {
+    const [now, count] = queue.dequeue();
+    if(now < 0 || now > 100_000 || visited.has(now)) continue; 
+    if(now === k) {
+        answer = Math.min(answer, count);
+        break;
+    }
+    
+    visited.add(now);
+    
+    queue.enqueue([now - 1, count + 1]);
+    queue.enqueue([now + 1, count + 1]);
+    queue.enqueue([now * 2, count + 1]);
+}
+
+console.log(answer);
