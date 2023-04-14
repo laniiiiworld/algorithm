2a1c3929136c130963dbb15b0b54d9c622cf0e5e
Lani <laniiiiworld@gmail.com>
2023-04-14 14:03:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 21.72 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,21 +1,49 @@
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.front = 0;
+        this.rear = 0;
+    }
+    size() {
+        return this.rear - this.front;
+    }
+    enqueue(value) {
+        this.queue.push(value);
+        this.rear += 1;
+    }
+    dequeue() {
+        const value = this.queue[this.front];
+        delete this.queue[this.front++];
+        return value;
+    }
+}
+
 function solution(maps) {
-    const n = maps.length - 1;
-    const m = maps[0].length - 1;
-    const queue = [[0, 0, 1]];
+    const n = maps.length;
+    const m = maps[0].length;
+    let answer = n * m + 1;
     
-    while(queue.length) {
-        const [row, col, count] = queue.shift();
+    const queue = new Queue();
+    queue.enqueue([0, 0, 1]);
+    maps[0][0] = 0;
+    
+    while(queue.size()) {
+        const [y, x, count] = queue.dequeue();
         
-        if(!maps[row][col]) continue;
-        if(row === n && col === m) return count;
+        if(y === n - 1 && x === m - 1) {
+            answer = Math.min(answer, count);
+            continue;
+        }
         
-        maps[row][col] = 0;
-        
-        if(row+1 <= n) queue.push([row+1, col, count+1]);
-        if(col+1 <= m) queue.push([row, col+1, count+1]);
-        if(row-1 >= 0) queue.push([row-1, col, count+1]);
-        if(col-1 >= 0) queue.push([row, col-1, count+1]);
+        for(const [plusY, plusX] of [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
+            const nextY = y + plusY;
+            const nextX = x + plusX;
+            if(nextY < 0 || nextX < 0 || nextY === n || nextX === m) continue;
+            if(maps[nextY][nextX] === 0) continue;
+            maps[nextY][nextX] = 0;
+            queue.enqueue([nextY, nextX, count + 1]);
+        }
     }
     
-    return -1;
+    return answer === n * m + 1 ? -1 : answer;
 }
