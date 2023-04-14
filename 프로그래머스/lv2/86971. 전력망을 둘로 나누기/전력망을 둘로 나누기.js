d34ad9369020ff0d33bc557f2026849d118e39f0
Lani <laniiiiworld@gmail.com>
2023-04-14 17:11:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 9.09 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,31 +1,59 @@
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.front = 0;
+        this.rear = 0;
+    }
+    size() {
+        return this.rear - this.front;
+    }
+    enqueue(value) {
+        this.queue.push(value);
+        this.rear += 1;
+    }
+    dequeue() {
+        const value = this.queue[this.front];
+        delete this.queue[this.front++];
+        return value;
+    }
+}
+
 function solution(n, wires) {
     let answer = n;
-    const getNewNetwork = (graph, start, visited) => {
-        const queue = [start];
-        visited[start] = true;
+    const bfs = (start, wires) => {
+        let result = 0;
+        const visited = Array(n + 1).fill(false);
+        const graph = Array.from({length: n + 1}, () => []);
+        for(const [v1, v2] of wires) {
+            graph[v1].push(v2);
+            graph[v2].push(v1);
+        }
         
-        while(queue.length) {
-            const value = queue.shift();
-            for(const i of graph[value]) {
-                if(visited[i]) continue;
-                queue.push(i);
-                visited[i] = true;
+        const queue = new Queue();
+        queue.enqueue(start);
+        
+        while(queue.size()) {
+            const now = queue.dequeue();
+            
+            if(visited[now]) continue;
+            visited[now] = true;
+            
+            result += 1;
+            
+            for(const end of graph[now]) {
+                if(visited[end]) continue;
+                queue.enqueue(end);
             }
         }
         
-        return visited.filter(value => value === true).length;
+        return result;
     };
     
-    for(let i=0; i<wires.length; i++) {
-        const graph = Array.from({length: n+1}, () => []);
-        const visited = Array(n+1).fill(false);
-        const copyWires = [...wires.slice(0, i), ...wires.slice(i+1)];
-        for(const [start, end] of copyWires) {
-            graph[start].push(end);
-            graph[end].push(start);
-        }
-        const count = getNewNetwork(graph, copyWires[0][0], visited);
-        answer = Math.min(answer, Math.abs(count - (n - count)));
+    for(let i = 0; i < wires.length; i++) {
+        const newWires = [...wires.slice(0, i), ...wires.slice(i + 1)];
+        const count = bfs(1, newWires);
+        const gap = Math.abs(count - (n - count));
+        answer = Math.min(answer, gap);
     }
     
     return answer;
