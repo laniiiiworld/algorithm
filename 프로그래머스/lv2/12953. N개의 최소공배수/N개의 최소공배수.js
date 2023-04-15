f70e78db35a90215500b06d0ddf36527878676ec
Lani <laniiiiworld@gmail.com>
2023-04-16 07:23:54 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,11 +1,14 @@
 function solution(arr) {
-    const getGCD = (a, b) => {
-        if(a < b) [a, b] = [b, a];
+    const getLCM = (a, b, ab = a * b) => {
         while(a % b) {
             [a, b] = [b, a % b];
         }
-        return b;
+        return ab / b;
     };
+    let lcm = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+        lcm = getLCM(lcm, arr[i]);
+    }
     
-    return arr.reduce((lcm, number) => lcm *= number/getGCD(lcm, number), 1);
+    return lcm;
 }
