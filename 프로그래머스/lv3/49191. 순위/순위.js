14211f640feb06e7d1cd570c7c680e9abbb6e3a7
Lani <laniiiiworld@gmail.com>
2023-04-17 22:26:05 +0900

    [level 3] Title: 순위, Time: 9.18 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.js
+++ 프로그래머스/lv3/49191. 순위/순위.js
@@ -1,28 +1,22 @@
 function solution(n, results) {
-    // gragh[a][b]
-    //  0: 경기 정보가 없다.
-    //  1: a가 b에게 이겼다.
-    // -1: a가 b에게 졌다.
-    const gragh = Array.from({length: n+1}, () => Array(n+1).fill(0));
-    for (const [won,lost] of results) {
-        gragh[won][lost] = 1;
-        gragh[lost][won] = -1;
+    const graph = Array.from({length: n + 1}, () => Array(n + 1).fill(0));
+    for(const [won, lost] of results) {
+        graph[won][lost] = 1;
+        graph[lost][won] = -1;
     }
     
-    //플로이드 워샬 알고리즘
-    for (let k=1; k<=n; k++) {
-        for (let a=1; a<=n; a++) {
-            for (let b=1; b<=n; b++) {
-                if (gragh[a][b] !== 0) continue;
-                // 경기정보가 입력되지 않은 경우 경기결과 유추
-                if (gragh[a][k] === 1 && gragh[k][b] === 1) {
-                    gragh[a][b] = 1;
-                }else if (gragh[a][k] === -1 && gragh[k][b] === -1) {
-                    gragh[a][b] = -1;
+    for(let k = 1; k <= n; k++) {
+        for(let a = 1; a <= n; a++) {
+            for(let b = 1; b <= n; b++) {
+                if(graph[a][b] !== 0) continue;
+                if(graph[a][k] === 1 && graph[k][b] === 1) {
+                    graph[a][b] = 1;
+                } else if(graph[a][k] === -1 && graph[k][b] === -1) {
+                    graph[a][b] = -1;
                 }
             }
         }
     }
     
-    return gragh.filter(a => a.filter(b => !b).length === 2).length;
+    return graph.filter(row => row.filter(v => v === 0).length === 2).length;
 }
