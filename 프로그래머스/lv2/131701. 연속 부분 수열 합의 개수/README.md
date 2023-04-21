d92ba8a2b12dd64520532b1da7fd830e20acff80
Lani <laniiiiworld@gmail.com>
2023-04-21 20:09:44 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 62.83 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 56.9 MB, 시간: 116.96 ms
+메모리: 57.1 MB, 시간: 62.83 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,12 +1,20 @@
 function solution(elements) {
-    const circular = elements.concat(elements);
-    const set = new Set();
-    for (let i = 0; i < elements.length; i++) {
+    const answer = new Set(elements);
+    const n = elements.length;
+    const arr = [...elements, ...elements];
+    for(let i = 2; i <= n; i++) {
+        let index = i;
         let sum = 0;
-        for (let j = 0; j < elements.length; j++) {
-            sum += circular[i + j];
-            set.add(sum);
+        while(index--) {
+            sum += arr[index];
+        }
+        answer.add(sum);
+        for(let j = i; j < arr.length; j++) {
+            sum += arr[j];
+            sum -= arr[j - i];
+            answer.add(sum);
         }
     }
-    return set.size;
+    
+    return answer.size;
 }
