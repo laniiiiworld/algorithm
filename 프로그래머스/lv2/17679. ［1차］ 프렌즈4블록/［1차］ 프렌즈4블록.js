90595840eb9f50f9e16498878ddc67e65df738d3
Lani <laniiiiworld@gmail.com>
2023-05-02 21:55:42 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 6.60 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,59 +1,67 @@
 function solution(m, n, board) {
-    const stack = []; // 2X2 삭제 가능한 블럭들
-    let isChanged = true;
-    
-    //board를 반시계방향으로 90도 회전시킨 board
-    let copyBoard = new Array(n)
-                        .fill(new Array(m).fill(''))
-                        .map((item, y) => item.map((v, x) => board[x][y] ));
-    
-    // 2X2 삭제 가능한지 확인하는 함수
-    const isRemove = (i,j) => {
-        const now = copyBoard[i][j];
-        
-        if(copyBoard[i][j+1] !== now) return false;
-        if(copyBoard[i+1][j] !== now) return false;
-        if(copyBoard[i+1][j+1] !== now) return false;
-        
-        return true;
-    };
-    
-    //블럭 배치를 업데이트하는 함수
-    const setCopyBoard = () => {
-        // stack에 있는 블럭들 2X2 삭제
-        while(stack.length) {
-            const [i, j] = stack.pop();
-            copyBoard[i][j] = '0';
-            copyBoard[i][j+1] = '0';
-            copyBoard[i+1][j] = '0';
-            copyBoard[i+1][j+1] = '0';
+    const graph = Array.from({length: n}, () => []);
+    for(let i = m - 1; i >= 0; i--) {
+        const row = board[i].split('');
+        for(let j = 0; j < n; j++) {
+            graph[j].push(row[j]);
         }
-        //빈 공간 채우기
-        copyBoard = copyBoard.map(item => item.join('')
-                                  .replaceAll('0','')
-                                  .padStart(m, '0')
-                                  .split('')
-                                 );
-    };
+    }
     
-    while(isChanged) {
-        isChanged = false;
+    const stack = [];
+    const coordinates = [[0, 0], [1, 0], [1, 1], [0, 1]];
+    const findRemoveBlocks = () => {
+        let isFind = false;
         
-        for(let i=0; i< n-1; i++) {
-            for(let j=0; j<m-1; j++) {
-                if(copyBoard[i][j] === '0') continue;
-                if(!isRemove(i,j)) continue;
+        for(let y = 0; y < n; y++) {
+            for(let x = 0; x < m; x++) {
+                if(graph[y][x] === ' ') continue;
+                if(y + 1 === n || x + 1 === m) continue;
                 
-                isChanged = true;
-                stack.push([i, j]);
+                let isEqual = true;
+                const nowBlock = graph[y][x];
+                for(const [plusX, plusY] of coordinates) {
+                    if(graph[y][x] === ' ') {
+                        isEqual = false;
+                        break;
+                    }
+                    if(plusY === n || plusX === m) {
+                        isEqual = false;
+                        break;
+                    }
+                    const nextX = x + plusX;
+                    const nextY = y + plusY;
+                    if(graph[nextY][nextX] !== nowBlock) {                        
+                        isEqual = false;
+                        break;
+                    }
+                }
+                if(!isEqual) continue;
+                for(const [plusX, plusY] of coordinates) {
+                    const nextX = x + plusX;
+                    const nextY = y + plusY;
+                    stack.push([nextX, nextY]);
+                    isFind = true;
+                }
             }
         }
         
-        setCopyBoard();
+        return isFind;
+    };
+    
+    const removeBlocks = () => {
+        while(stack.length) {
+            const [x, y] = stack.pop();
+            graph[y][x] = '';
+        }
+        
+        for(let i = 0; i < n; i++) {
+            graph[i] = graph[i].join('').padEnd(m, ' ').split('');
+        }
+    };
+    
+    while(findRemoveBlocks()) {
+        removeBlocks();
     }
     
-    return copyBoard.reduce((acc, cur) => {
-                        acc += cur.filter(v => v === '0').length;
-                        return acc;
-                    }, 0);
+    return n*m - graph.reduce((sum, row) => sum += row.join('').replaceAll(' ', '').length, 0);
 }
