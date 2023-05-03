2d528af056dbf886def6ea0985a9bd3069efd1c7
Lani <laniiiiworld@gmail.com>
2023-05-04 08:58:01 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 628 ms, Memory: 11184 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -1,37 +1,41 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const baseOperation = ['+', '-', '*', '/'];
 const n = Number(input[0]);
 const numbers = input[1].split(' ').map(Number);
-const operations = ['+', '-', '*', '/'];
-const usedOperations = [];
-input[2].split(' ').forEach((count, index) => {
-    usedOperations.push(...operations[index].repeat(Number(count)).split(''));
+const visited = Array(n - 1).fill(false); //operation 사용여부
+const operations = [''];
+let max = -Infinity;
+let min = Infinity;
+
+input[2].split(' ').map(Number).forEach((v, i) => {
+    for(const o of baseOperation[i].repeat(v)) {
+        operations.push(o);
+    }
 });
 
-let maxAnswer = -Infinity;
-let minAnswer = Infinity;
-const visited = Array(usedOperations.length).fill(false);
-const calculate = (operation, a, b) => {
-    if(operation === '') return a;
+const calculateNumbers = (a, b, operation) => {
     if(operation === '+') return a + b;
     if(operation === '-') return a - b;
     if(operation === '*') return a * b;
     return ~~(a / b);
 };
-const dfs = (operation, a, b, index) => {
-    a = calculate(operation, a, b);
-    if(index === numbers.length) {
-        if(maxAnswer < a) maxAnswer = a;
-        if(a < minAnswer) minAnswer = a;
+
+const dfs = (depth, result) => {
+    if(depth === n) {
+        max = Math.max(max, result);
+        min = Math.min(min, result);
         return;
     }
-    for(let i=0; i<usedOperations.length; i++) {
+    
+    for(let i = 1; i < n; i++) {
         if(visited[i]) continue;
         visited[i] = true;
-        dfs(usedOperations[i], a, numbers[index], index + 1);
+        dfs(depth + 1, calculateNumbers(result, numbers[depth], operations[i]));
         visited[i] = false;
     }
 };
 
-dfs('', numbers[0], 0, 1);
+dfs(1, numbers[0]);
 
-console.log(`${maxAnswer}\n${minAnswer}`);
+console.log(max);
+console.log(min);
