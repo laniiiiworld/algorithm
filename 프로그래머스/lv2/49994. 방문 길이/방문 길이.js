58083315c8b0412d0d5e61f17ce5f2fcaac9f60e
Lani <laniiiiworld@gmail.com>
2023-05-07 13:54:06 +0900

    [level 2] Title: 방문 길이, Time: 0.63 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -1,24 +1,21 @@
 function solution(dirs) {
-    const directions = {'U': [1, 0], 'D': [-1, 0], 'L': [0, -1], 'R': [0, 1]};
+    let [y, x] = [0, 0];
     const visited = new Set();
-    const dfs = (x, y, index) => {
-        if(index === dirs.length) return;
+    const coordinates = {'U': [1, 0], 'D': [-1, 0], 'R': [0, -1], 'L': [0, 1]};
+    
+    for(let i = 0; i < dirs.length; i++) {
+        const [plusY, plusX] = coordinates[dirs[i]];
+        const nextY = y + plusY;
+        const nextX = x + plusX;
         
-        const [plusY, plusX] = directions[dirs[index]];
-        let nextX = x + plusX;
-        let nextY = y + plusY;
-        if(nextX < -5 || nextX > 5) {
-            nextX = x;
-        }else if(nextY < -5 || nextY > 5) {
-            nextY = y;
-        } else {
-            visited.add(`${Math.min(x, nextX)} ${Math.min(y, nextY)} ${Math.max(x, nextX)} ${Math.max(y, nextY)}`);
+        if(nextX < -5 || nextY < -5 || nextX > 5 || nextY > 5) continue;
+        const checked = `${Math.min(y, nextY)}${Math.max(y, nextY)}${Math.min(x, nextX)}${Math.max(x, nextX)}`;
+        if(!visited.has(checked)) {
+            visited.add(checked);
         }
         
-        dfs(nextX, nextY, index + 1);
-    };
-    
-    dfs(0, 0, 0);
+        [y, x] = [nextY, nextX];
+    }
     
     return visited.size;
 }
