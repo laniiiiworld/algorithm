5813b7d0a67384ad745805c1e87af58280130f86
Lani <laniiiiworld@gmail.com>
2023-05-19 12:26:03 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -2,45 +2,43 @@ class MinHeap {
     constructor() {
         this.heap = [null];
     }
-    isEmpty() {
-        return this.heap.length === 1;
+    size() {
+        return this.heap.length - 1;
     }
-    push(item) {
-        this.heap.push(item);
+    push(value) {
+        this.heap.push(value);
         
         let currentIndex = this.heap.length - 1;
-        let parentIndex = Math.floor(currentIndex / 2);
+        let parentIndex = ~~(currentIndex / 2);
         
-        while(currentIndex > 1 && this.heap[currentIndex][1] < this.heap[parentIndex][1]) {
+        while(parentIndex > 0 && this.heap[currentIndex][1] < this.heap[parentIndex][1]) {
             this._swap(currentIndex, parentIndex);
             currentIndex = parentIndex;
-            parentIndex = Math.floor(currentIndex / 2);
+            parentIndex = ~~(currentIndex / 2);
         }
     }
     pop() {
-        if(this.isEmpty()) return null;
-        if(this.heap.length === 2) return this.heap.pop();
+        if(this.size() === 1) return this.heap.pop();
         
-        const returnItem = this.heap[1];
+        const returnValue = this.heap[1];
         this.heap[1] = this.heap.pop();
         
         let currentIndex = 1;
         let leftIndex = 2;
         let rightIndex = 3;
         
-        if(!this.heap[leftIndex]) return returnItem;
-        if(!this.heap[rightIndex]) {
+        if(this.size() === 1) {
+            return returnValue;
+        } else if(this.size() === 2) {
             if(this.heap[leftIndex][1] < this.heap[currentIndex][1]) {
                 this._swap(leftIndex, currentIndex);
             }
-            return returnItem;
+            return returnValue;
         }
         
-        while(
-              (this.heap[leftIndex][1] < this.heap[currentIndex][1]) || 
-              (this.heap[rightIndex][1] < this.heap[currentIndex][1])
-            ) {
-            const minIndex = this.heap[leftIndex][1] < this.heap[rightIndex][1] ? leftIndex : rightIndex;
+        while((this.heap[leftIndex][1] < this.heap[currentIndex][1]) ||
+              (this.heap[rightIndex][1] < this.heap[currentIndex][1])) {
+            const minIndex = (this.heap[leftIndex][1] <= this.heap[rightIndex][1])? leftIndex : rightIndex;
             this._swap(minIndex, currentIndex);
             currentIndex = minIndex;
             leftIndex = currentIndex * 2;
@@ -48,7 +46,7 @@ class MinHeap {
             if(leftIndex >= this.heap.length - 1) break;
         }
         
-        return returnItem;
+        return returnValue;
     }
     _swap(a, b) {
         [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
@@ -56,31 +54,29 @@ class MinHeap {
 }
 function solution(jobs) {
     jobs.sort((a, b) => b[0] - a[0] || b[1] - a[1]);
-    const length = jobs.length;
-    const heap = new MinHeap(); // 대기열
-    let time = 0; // 현재 시간
-    let totalWorkTime = 0; // 현재까지 작업에 소요된 시간
     
-    // 현재 요청시간이 지난 작업들을 대기열에 추가
-    const addNewJobs = () => {
+    const length = jobs.length;
+    const heap = new MinHeap();
+    let totalWorkTime = 0;
+    let time = 0;
+    
+    while(jobs.length || heap.size()) {
+        if(jobs.length && heap.size() === 0) {
+            const [requestTime, workTime] = jobs.pop();
+            time = Math.max(requestTime, time);
+            heap.push([requestTime, workTime]);
+        }
+        
         while(jobs.length && jobs[jobs.length - 1][0] <= time) {
             heap.push(jobs.pop());
         }
-    };
-    
-    while(jobs.length || !heap.isEmpty()) {
-        addNewJobs();
         
-        if(heap.isEmpty()) {
-            time = jobs[jobs.length - 1][0];
-            addNewJobs();
-        }
+        if(heap.size() === 0) continue;
         
-        const [startTime, workTime] = heap.pop();
-        const waitingTime = time - startTime;
+        const [requestTime, workTime] = heap.pop();
+        totalWorkTime += time - requestTime + workTime;
         time += workTime;
-        totalWorkTime += waitingTime + workTime;
     }
     
-    return Math.floor(totalWorkTime / length);
+    return ~~(totalWorkTime / length);
 }
