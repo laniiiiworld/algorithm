1eba169950890dfe9eea27c0bea6c05120259814
Lani <laniiiiworld@gmail.com>
2023-05-20 23:12:42 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.js
+++ 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.js
@@ -1,11 +1,15 @@
 function solution(n) {
+    if(n === 1) return 1;
+    if(n === 2) return 2;
+    
     const dp = Array(n + 1).fill(0);
     dp[0] = 1;
     dp[1] = 1;
     for(let i = 2; i <= n; i++) {
-        for(let j = 1; j <= i; j++) {
-            dp[i] += dp[i - j] * dp[j - 1];
+        for(let j = 0; j < i; j++) {
+            dp[i] += dp[j] * dp[i - j - 1];
         }
     }
+    
     return dp[n];
 }
