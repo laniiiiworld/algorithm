b3d4d31fa3d1c4859cdd9a4780450059edac5575
Lani <laniiiiworld@gmail.com>
2023-05-20 19:45:31 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.41 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -3,7 +3,7 @@ function solution(n) {
     dp[1] = 1;
     dp[2] = 2;
     for(let i = 3; i <= n; i++) {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1_000_000_007;
     }
     
     return dp[n];
