42846fe46f0aa6b5d4637d152e70dbe384535276
Lani <laniiiiworld@gmail.com>
2023-05-21 10:36:52 +0900

    [level 3] Title: N으로 표현, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
@@ -1,21 +1,23 @@
 function solution(N, number) {
     const MAX = 8;
-    const dp = Array.from({length: MAX + 1}, (_, index) => {
-        const repeatedNumber = Number(String(N).repeat(index));
-        return new Set([repeatedNumber]);
+    const dp = Array.from({length: MAX + 1}, (_, i) => {
+        const value = Number(String(N).repeat(i));
+        const mySet = new Set();
+        mySet.add(value);
+        return mySet;
     });
     
     for(let i = 1; i <= MAX; i++) {
         for(let j = 1; j < i; j++) {
-            for(let first of dp[j]) {
-                for(let second of dp[i-j]) {
-                    dp[i].add(first+second);
-                    dp[i].add(first-second);
-                    dp[i].add(first*second);
+            for(const first of dp[j]) {
+                for(const second of dp[i - j]) {
+                    dp[i].add(first + second);
+                    dp[i].add(first - second);
+                    dp[i].add(first * second);
                     if(first === 0 || second === 0) continue;
                     dp[i].add(Math.floor(first/second));
                 }
-            }
+            } 
         }
         if(dp[i].has(number)) return i;
     }
