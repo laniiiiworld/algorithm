01947bbf1b51cf674d1e7900b20a597636a060da
Lani <laniiiiworld@gmail.com>
2023-06-09 12:38:38 +0900

    [unrated] Title: 인사고과, Time: 55.94 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/unrated/152995. 인사고과/인사고과.js
+++ 프로그래머스/unrated/152995. 인사고과/인사고과.js
@@ -1,11 +1,11 @@
 function solution(scores) {
     const wanho = scores[0];
-    let answer = 1;
-    let maxScore = 0;
     const wanhoSum = wanho[0] + wanho[1];
-
-    scores.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]);
+    let maxScore = 0;
+    let answer = 1;
     
+    scores.sort((a, b) => b[0] - a[0] || a[1] - b[1]);
+
     for(let score of scores){
         if(score[1] < maxScore) {
             if(score === wanho) return -1;
@@ -16,6 +16,5 @@ function solution(scores) {
             }
         }
     }
-    
     return answer;
 }
