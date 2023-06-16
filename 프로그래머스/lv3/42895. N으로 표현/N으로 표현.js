6f7dd83b37c36fbb0689f82077e7ccfcdff608e4
Lani <laniiiiworld@gmail.com>
2023-06-16 15:10:29 +0900

    [level 3] Title: N으로 표현, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
@@ -1,25 +1,26 @@
 function solution(N, number) {
     const MAX = 8;
     const dp = Array.from({length: MAX + 1}, (_, i) => {
-        const value = Number(String(N).repeat(i));
         const mySet = new Set();
-        mySet.add(value);
+        mySet.add(Number(String(N).repeat(i)));
         return mySet;
     });
     
-    for(let i = 1; i <= MAX; i++) {
-        for(let j = 1; j < i; j++) {
-            for(const first of dp[j]) {
-                for(const second of dp[i - j]) {
-                    dp[i].add(first + second);
-                    dp[i].add(first - second);
-                    dp[i].add(first * second);
-                    if(first === 0 || second === 0) continue;
-                    dp[i].add(Math.floor(first/second));
+    for(let count = 1; count <= MAX; count++) {
+        for(let i = count - 1; i > 0; i--) {
+            for(const a of dp[i]) {
+                for(const b of dp[count - i]) {
+                    const calculated = [a + b, a - b, a * b, Math.floor(a / b)];
+                    for(const result of calculated) {
+                        dp[count].add(result);
+                    }
                 }
-            } 
+            }
         }
-        if(dp[i].has(number)) return i;
+        
+        if(dp[count].has(number)) {
+            return count;
+        }
     }
     
     return -1;
