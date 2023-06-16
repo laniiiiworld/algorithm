d908e39a92f2d5aa06de9abfad13e46d43d3365f
Lani <laniiiiworld@gmail.com>
2023-06-16 11:44:33 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,9 @@
 function solution(n) {
     const dp = Array(n + 1).fill(0);
+    dp[0] = 1;
     dp[1] = 1;
-    dp[2] = 2;
-
-    for(let i = 3; i <= n; i++) {
+    
+    for(let i = 2; i <= n; i++) {
         dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
     }
     
