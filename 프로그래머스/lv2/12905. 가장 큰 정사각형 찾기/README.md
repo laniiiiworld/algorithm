58cf185187246934aacb1355b8c90b5c6fe18c4a
Lani <laniiiiworld@gmail.com>
2023-06-16 16:29:04 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 32.99 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/README.md
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 정사각형 찾기 - 12905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905?language=javascript) 
 
 ### 성능 요약
 
-메모리: 81.9 MB, 시간: 49.45 ms
+메모리: 82.3 MB, 시간: 32.99 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -2,13 +2,17 @@ function solution(board) {
     const n = board.length;
     const m = board[0].length;
     
-    for(let i = 1; i < n; i++) {
-        for(let j = 1; j < m; j++) {
-            if(!board[i - 1][j - 1] || !board[i - 1][j] || !board[i][j - 1] || !board[i][j]) continue;
-            board[i][j] = Math.min(board[i - 1][j - 1], board[i - 1][j], board[i][j - 1]) + 1;
+    if(Math.min(n, m) === 1) return Math.max(...board.flat());
+    
+    let answer = 1;
+    
+    for(let row = 1; row < n; row++) {
+        for(let col = 1; col < m; col++) {
+            if(!board[row][col] || !board[row - 1][col] || !board[row][col - 1] || !board[row - 1][col - 1]) continue;
+            board[row][col] = Math.min(board[row - 1][col], board[row][col - 1], board[row - 1][col - 1]) + 1;
+            answer = Math.max(answer, board[row][col]);
         }
     }
     
-    const value = board.reduce((acc, row) => acc = Math.max(acc, ...row), 0);
-    return value * value;
+    return Math.max(...board.map(row=>Math.max(...row)))**2;
 }
