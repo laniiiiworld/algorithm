d44d43bb38aa249d143decd45e096f05f825e9b3
Lani <laniiiiworld@gmail.com>
2023-06-19 10:05:16 +0900

    [level 2] Title: 피보나치 수, Time: 3.10 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,9 +1,11 @@
 function solution(n) {
-    const dp = [0, 1, 1];
-    let x = 2;
-    while(x < n) {
-        x += 1;
-        dp[x] = (dp[x-1] + dp[x-2]) % 1234567;
+    const dp = Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 1;
+    
+    for(let i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
     }
-    return dp[x];
+    
+    return dp[n];
 }
