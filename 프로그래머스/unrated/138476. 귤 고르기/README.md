72bfac2617899681774a6ac0c8eb5117a8d33e28
Lani <laniiiiworld@gmail.com>
2023-06-19 13:21:46 +0900

    [unrated] Title: 귤 고르기, Time: 42.38 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/README.md
+++ 프로그래머스/unrated/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 58.2 MB, 시간: 53.78 ms
+메모리: 55.1 MB, 시간: 42.38 ms
 
 ### 구분
 
--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,74 +1,17 @@
-class MaxHeap {
-    constructor() {
-        this.heap = [null];
-    }
-    push(item) {
-        this.heap.push(item);
-        
-        let currentIndex = this.heap.length - 1;
-        let parentIndex = Math.floor(currentIndex / 2);
-        
-        while(parentIndex >= 1 && this.heap[parentIndex].count < this.heap[currentIndex].count) {
-            this._swap(parentIndex, currentIndex);
-            currentIndex = parentIndex;
-            parentIndex = Math.floor(currentIndex / 2);
-        }
-    }
-    pop() {
-        if(this.heap.length === 1) return null;
-        if(this.heap.length === 2) return this.heap.pop();
-        
-        const returnItem = this.heap[1];
-        this.heap[1] = this.heap.pop();
-        
-        let currentIndex = 1;
-        let leftIndex = currentIndex * 2;
-        let rightIndex = currentIndex * 2 + 1;
-        if(!this.heap[leftIndex]) {
-            return returnItem;
-        } else {
-            if(!this.heap[rightIndex]) {
-                if(this.heap[currentIndex].count < this.heap[leftIndex].count) {
-                    this._swap(currentIndex, leftIndex);
-                }
-                return returnItem;
-            }
-        }
-        
-        while((leftIndex < this.heap.length - 1) &&
-              (
-                (this.heap[currentIndex].count < this.heap[leftIndex].count) ||
-                (this.heap[currentIndex].count < this.heap[rightIndex].count)
-              )) {
-            const maxIndex = (this.heap[leftIndex].count < this.heap[rightIndex].count) ? rightIndex : leftIndex;
-            this._swap(maxIndex, currentIndex);
-            currentIndex = maxIndex;
-            leftIndex = currentIndex * 2;
-            rightIndex = currentIndex * 2 + 1;
-        }
-        
-        return returnItem;
-    }
-    _swap(a, b) {
-        [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
-    }
-}
 function solution(k, tangerine) {
-    const myMap = new Map();
-    const heap = new MaxHeap();
+    const types = new Map();
+    
+    for(const value of tangerine) {
+        const count = types.get(value) || 0;
+        types.set(value, count + 1);
+    }
+    
+    const sorted = [...types].sort((a, b) => a[1] - b[1]);
     let answer = 0;
-
-    for(const size of tangerine) {
-        const count = myMap.get(size) || 0;
-        myMap.set(size, count + 1);
-    }
+    let poped = 0;
     
-    for(const size of myMap.keys()) {
-        heap.push({size, count : myMap.get(size)});
-    }
-    
-    while(k > 0) {
-        k -= heap.pop().count;
+    while(poped < k) {
+        poped += sorted.pop()[1];
         answer += 1;
     }
     
