5f116e5ba29598c1940d8c11478af539f0dfbb35
Lani <laniiiiworld@gmail.com>
2023-06-19 14:11:30 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,38 +1,35 @@
-//올바른 괄호 여부 확인 함수
-function isCorrect(str) {
-    const all = ['(','[','{',')',']','}'];
-    const left = ['(','[','{'];
-    const sum = [0,0,0];
-    let stack = []; //열어주는 괄호들의 index를 저장하는 배열
-
-    for(let i=0; i<str.length; i++) {
-        const index = all.indexOf(str[i]) % 3;
-
-        if(left.indexOf(str[i]) > -1) {
-            sum[index] += 1;
-            stack.push(index);
-        } else {
-            //괄호를 열기 전에 닫으려 하는 경우 X
-            if(stack.length === 0) return 0;
-            //열어주는 괄호와 짝이 맞지 않은 경우 X
-            if(index !== stack[stack.length-1]) return 0;
-            sum[index] -= 1;
-            stack.pop();
-        }
-    }
-
-    //열어주는 괄호들이 모두 닫히지 않은 경우 X
-    return sum.filter(value => value !== 0).length? 0 : 1;
-}
-
 function solution(s) {
-    const sLength = s.length;
-    let count =0;
-    s += s;
+    const isRight = (text) => {
+        const indexs = {'[': 0, ']': 0, '(': 1, ')': 1, '{': 2, '}': 2};
+        const values = {'[': [1, 0, 0], '(': [0, 1, 0], '{': [0, 0, 1]};
+        const stack = [];
+        const result = [0, 0, 0];
 
-    for(let start=0; start<sLength; start++) {
-        count += isCorrect(s.slice(start, start + sLength));
+        for(let i = 0; i < text.length; i++) {
+            const value = text[i];
+            const index = indexs[value];
+            const last = stack[stack.length - 1];
+            
+            if(value === '[' || value === '(' || value === '{') {
+                stack.push(values[value]);
+                result[index] += 1;
+            } else {
+                if(stack.length === 0) return false;
+                if(last[index] === 0) return false;
+                stack.pop();
+                result[index] -= 1;
+            }
+        }
+        
+        return result.reduce((acc, cur) => acc += cur, 0) === 0;
+    };
+    
+    let answer = 0;
+    
+    for(let i = 0; i < s.length; i++) {
+        if(!isRight(s.slice(i) + s.slice(0, i))) continue;
+        answer += 1;
     }
-
-    return count;
+    
+    return answer;
 }
