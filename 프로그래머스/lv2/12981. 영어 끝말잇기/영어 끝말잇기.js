a077e090c3fc78af45e25e464c1de9c140b0add6
Lani <laniiiiworld@gmail.com>
2023-06-19 10:26:53 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,22 +1,15 @@
 function solution(n, words) {
-    const calledWords = new Set();
-    let lastLetter = words[0].charAt(words[0].length - 1);
-    let i=1;
+    const setAnswer = (index) => [(index) % n + 1, Math.ceil((index + 1) / n)];
+    const talked = new Set();
+    let last = words[0][0];
     
-    calledWords.add(words[0]);
-    
-    while(i < words.length) {
+    for(let i = 0; i < words.length; i++) {
         const word = words[i];
-        //규칙1 앞사람이 말한 단어의 마지막 문자로 시작
-        //규칙2 이전에 등장했던 단어 사용X
-        if(word.charAt(0) !== lastLetter || calledWords.has(word)) {
-            return [i % n + 1, Math.ceil((i + 1) / n)];
-        }
+        if(word[0] !== last) return setAnswer(i);
+        if(talked.has(word)) return setAnswer(i);
         
-        lastLetter = word.charAt(word.length - 1);
-        calledWords.add(word);
-        
-        i += 1;
+        last = word[word.length - 1];
+        talked.add(word);
     }
     
     return [0, 0];
