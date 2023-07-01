397c1586d205422e1c245ae7119b8aed68214527
Lani <laniiiiworld@gmail.com>
2023-07-01 21:25:51 +0900

    [level 2] Title: 기능개발, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,16 +1,19 @@
 function solution(progresses, speeds) {
-    let before = 0;
-    return progresses
-                .map((progress, index) => {
-                    return Math.ceil((100 - progress) / speeds[index]);
-                })
-                .reduce((answer, day) => {
-                    if(day <= before) {
-                        answer[answer.length - 1]++;
-                    } else {
-                        before = day;
-                        answer = [...answer, 1];
-                    }
-                    return answer;
-                }, []);
+    const answer = [];
+    const days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+    
+    let count = 0;
+    let now = days[0];
+    for(const value of days) {
+        if(now >= value) {
+            count += 1;
+        } else {
+            answer.push(count);
+            count = 1;
+            now = value;
+        }
+    }
+    answer.push(count);
+    
+    return answer;
 }
