3d09867cfb906addef50bb894663256ed361f205
Lani <laniiiiworld@gmail.com>
2023-07-03 22:21:17 +0900

    [level 2] Title: 땅따먹기, Time: 41.37 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -1,13 +1,10 @@
 function solution(land) {
-    const n = land.length;
-    
-    for(let i = 1; i < n; i++) {
-        const [a, b, c, d] = land[i - 1];
-        land[i][0] += Math.max(b, c, d);
-        land[i][1] += Math.max(a, c, d);
-        land[i][2] += Math.max(a, b, d);
-        land[i][3] += Math.max(a, b, c);
-    }
-    
-    return Math.max(...land[n - 1]);
+    return Math.max(...land.reduce((sum, row) => {
+        return [
+            row[0] + Math.max(sum[1], sum[2], sum[3]),  
+            row[1] + Math.max(sum[0], sum[2], sum[3]),
+            row[2] + Math.max(sum[0], sum[1], sum[3]),
+            row[3] + Math.max(sum[0], sum[1], sum[2]),
+        ];
+    }, [0, 0, 0, 0]));
 }
