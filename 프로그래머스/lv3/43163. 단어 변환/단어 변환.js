0cc1d5dba3ed4ab7f8ddcc35034fd3eecbe06b4e
Lani <laniiiiworld@gmail.com>
2023-07-10 15:22:51 +0900

    [level 3] Title: 단어 변환, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,31 +1,32 @@
 function solution(begin, target, words) {
-    let answer = words.length + 1;
-    const visited = Array(words.length).fill(false);
-    const isChange = (before, after) => {
+    const n = words.length;
+    const m = begin.length;
+    const visited = Array(n).fill(false);
+    let answer = n;
+    
+    const canBeChanged = (before, after) => {
         let count = 0;
-        for(let i = 0; i < before.length; i++) {
-            if(before[i] !== after[i]) count += 1;
+        for(let i = 0; i < m; i++) {
+            if(before[i] === after[i]) continue;
+            count += 1;
         }
         return count === 1;
     };
-    const dfs = (before, count) => {
-        if(before === target) {
-            answer = Math.min(answer, count);
+    const dfs = (depth, text) => {
+        if(text === target) {
+            answer = Math.min(answer, depth);
             return;
         }
         
-        for(let i = 0; i < words.length; i++) {
-            if(visited[i]) continue;
-            if(!isChange(before, words[i])) continue;
+        for(let i = 0; i < n; i++) {
+            if(visited[i] || !canBeChanged(text, words[i])) continue;
             visited[i] = true;
-            dfs(words[i], count + 1);
+            dfs(depth + 1, words[i]);
             visited[i] = false;
         }
     };
     
-    if(!words.includes(target)) return 0;
+    dfs(0, begin);
     
-    dfs(begin, 0);
-    
-    return answer === words.length + 1 ? 0 : answer;
+    return answer === n ? 0 : answer;
 }
