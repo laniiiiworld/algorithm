f07572a65eb711113822b4598c5c0325433e528d
Lani <laniiiiworld@gmail.com>
2023-07-10 15:57:42 +0900

    [level 2] Title: 방문 길이, Time: 0.64 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -1,20 +1,33 @@
 function solution(dirs) {
-    let [y, x] = [0, 0];
+    let [nowX, nowY] = [0, 0];
     const visited = new Set();
-    const coordinates = {'U': [1, 0], 'D': [-1, 0], 'R': [0, -1], 'L': [0, 1]};
-    
-    for(let i = 0; i < dirs.length; i++) {
-        const [plusY, plusX] = coordinates[dirs[i]];
-        const nextY = y + plusY;
-        const nextX = x + plusX;
+    const canBeMoved = (x, y) => x >= -5 && x <= 5 && y >= -5 && y <= 5;
+    const moveDirection = (direction) => {
+        let nextX = nowX;
+        let nextY = nowY;
         
-        if(nextX < -5 || nextY < -5 || nextX > 5 || nextY > 5) continue;
-        const checked = `${Math.min(y, nextY)}${Math.max(y, nextY)}${Math.min(x, nextX)}${Math.max(x, nextX)}`;
-        if(!visited.has(checked)) {
-            visited.add(checked);
+        if(direction === 'U') {
+            nextY += 1;
+        } else if(direction === 'D') {
+            nextY -= 1;
+        } else if(direction === 'R') {
+            nextX += 1;
+        } else if(direction === 'L') {
+            nextX -= 1;
         }
         
-        [y, x] = [nextY, nextX];
+        if(!canBeMoved(nextX, nextY)) return [nowX, nowY];
+        return [nextX, nextY];
+    };
+    
+    for(let i = 0; i < dirs.length; i++) {
+        const [nextX, nextY] = moveDirection(dirs[i], nowX, nowY);
+        
+        if(nextX === nowX && nextY === nowY) continue;
+        visited.add(`${(nowX + nextX) / 2}${(nowY + nextY) / 2}`);
+        
+        nowX = nextX;
+        nowY = nextY;
     }
     
     return visited.size;
