0e420a1ea2f4b13966c56c7215afabbe6abe877c
Lani <laniiiiworld@gmail.com>
2023-07-12 21:04:32 +0900

    [level 3] Title: 단속카메라, Time: 4.68 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.js
@@ -1,10 +1,10 @@
 function solution(routes) {
     routes.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
-    const cameras = [routes[0][1]];
     
+    const cameras = [routes[0][1]];
     for(const [start, end] of routes) {
         const camera = cameras[cameras.length - 1];
-        if(camera >= end) {
+        if(end < camera) {
             cameras.pop();
             cameras.push(end);
         } else if(camera < start) {
