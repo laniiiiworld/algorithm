acd7bc632d1034b24b16b2caec2aa106b3ec89cb
Lani <laniiiiworld@gmail.com>
2023-07-12 21:51:55 +0900

    [level 3] Title: 숫자 게임, Time: 74.17 ms, Memory: 48.6 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
@@ -1,17 +1,19 @@
 function solution(A, B) {
     let count = 0;
+    let start = 0;
+    let end = B.length - 1;
     
     A.sort((a, b) => b - a);
     B.sort((a, b) => a - b);
     
     for(const value of A) {
-        if(value < B[0]) {
-            B.shift();
+        if(value < B[start]) {
+            start += 1;
             count += 1;
-        } else if(B[B.length - 1] <= value) {
-            B.shift();
+        } else if(B[end] <= value) {
+            start += 1;
         } else {
-            B.pop();
+            end -= 1;
             count += 1;
         }
     }
