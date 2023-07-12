e264ba5b3d5f28d0b442bb871a64e26ca6b00814
Lani <laniiiiworld@gmail.com>
2023-07-12 20:08:56 +0900

    [unrated] Title: 숫자 변환하기, Time: 1.87 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.js
@@ -7,38 +7,38 @@ class Queue {
     size() {
         return this.rear - this.front;
     }
-    enqueue(value) {
-        this.queue.push(value);
+    enqueue(item) {
+        this.queue.push(item);
         this.rear += 1;
     }
     dequeue() {
-        const value = this.queue[this.front];
+        const item = this.queue[this.front];
         delete this.queue[this.front++];
-        return value;
+        return item;
     }
 }
-
 function solution(x, y, n) {
-    let answer = Infinity;
-    const visited = new Set();
-    const queue = new Queue();
-    queue.enqueue([x, 0]);
-    visited.add(x);
-    
-    while(queue.size()) {
-        const [now, count] = queue.dequeue();
-        if(now === y) {
-            answer = count;
-            break;
+    const bfs = (start) => {
+        const visited = new Set();
+        const queue = new Queue();
+        queue.enqueue([start, 0]);
+        visited.add(start);
+        
+        while(queue.size()) {
+            const [now, count] = queue.dequeue();
+            
+            if(now === y) return count;
+            
+            for(const next of [now + n, now * 2, now * 3]) {
+                if(next > y || visited.has(next)) continue;
+                visited.add(next);
+                queue.enqueue([next, count + 1]);
+            }
         }
-                
-        for(const next of [now + n, now * 2, now * 3]) {
-            if(next > y) continue;
-            if(visited.has(next)) continue;
-            visited.add(next);
-            queue.enqueue([next, count + 1]);
-        }
-    }
+        
+        return -1;
+    };
     
-    return answer === Infinity? -1 : answer;
+    return bfs(x);
+    
 }
