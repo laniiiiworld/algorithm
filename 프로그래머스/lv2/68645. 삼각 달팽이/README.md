18fc2208621c8c983c4ebef7e4265de3a9d49b25
Lani <laniiiiworld@gmail.com>
2023-07-13 20:19:35 +0900

    [level 2] Title: 삼각 달팽이, Time: 103.44 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/README.md
+++ 프로그래머스/lv2/68645. 삼각 달팽이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 삼각 달팽이 - 68645 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 94.25 ms
+메모리: 106 MB, 시간: 103.44 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,15 +1,37 @@
 function solution(n) {
-    const numbers = Array.from({length: n}, (_, i) => new Array(i + 1).fill(0));
-    let [x, y] = [-1, 0];
-    let number = 0;
-    let count = 0;
+    const target = n * (n + 1) / 2;
+    const graph = Array.from({length: n}, (_, i) => Array(i + 1).fill(0));
+    const fillLeft = (value, row, col) => {
+        while(row < n) {
+            if(graph[row][col]) return [value, --row, ++col];
+            graph[row++][col] = value++;
+        }
+        
+        return [value, --row, ++col];
+    };
+    const fillBottom = (value, row, col) => {
+        while(col < n) {
+            if(graph[row][col]) return [value, --row, col - 2];
+            graph[row][col++] = value++;
+        }
+        
+        return [value, --row, col - 2];
+    };
+    const fillRight = (value, row, col) => {
+        while(row > 0 && col >= 0) {
+            if(graph[row][col]) return [value, row + 2, ++col];
+            graph[row--][col--] = value++;
+        }
+        
+        return [value, row + 2, ++col];
+    };
     
-    for (let row = n; row > 0; row -= 3) {
-        numbers[++x][y] = ++number;
-        for (count = 0; count < row-1; count++) numbers[++x][y] = ++number;
-        for (count = 0; count < row-1; count++) numbers[x][++y] = ++number;
-        for (count = 0; count < row-2; count++) numbers[--x][--y] = ++number;
+    let [value, row, col] = [1, 0, 0];
+    while(value <= target) {
+        [value, row, col] = fillLeft(value, row, col);
+        [value, row, col] = fillBottom(value, row, col);
+        [value, row, col] = fillRight(value, row, col);
     }
     
-    return numbers.flat();
+    return graph.flat();
 }
