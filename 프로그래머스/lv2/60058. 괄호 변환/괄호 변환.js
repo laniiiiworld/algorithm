d48d96a1d49b4ecb945ce2539382100239f6e50d
Lani <laniiiiworld@gmail.com>
2023-07-13 23:56:08 +0900

    [level 2] Title: 괄호 변환, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
@@ -1,36 +1,42 @@
 function solution(p) {
-    const isCorrect = (str) => {
-        let sum = 0;
-        for(let index = 0; index < str.length; index++) {
-            sum += (str[index] === '(')? 1 : -1;
-            if(sum < 0) return false;
+    const isRightString = (s) => {
+        let result = 0;
+        for(let i = 0; i < s.length; i++) {
+            if(result < 0) return false;
+            if(s[i] === '(') {
+                result += 1;
+            } else {
+                result -= 1;
+            }
         }
-        return sum === 0;
+        return result === 0;
     };
-    const separateString = (str) => {
-        let index = 0;
-        let sum = 0;
-        while(index < str.length) {
-            sum += (str[index++] === '(')? 1 : -1;
-            if(sum === 0) break;
+    const getBalancedStrings = (s) => {
+        let count = s[0] === '('? 1 : -1;
+        
+        for(let i = 1; i < s.length; i++) {
+            if(count === 0) return [s.slice(0, i), s.slice(i)];
+            count += s[i] === '('? 1 : -1;
         }
-        return [str.slice(0, index), str.slice(index)];
+        
+        return [s, ''];
     };
-    const getCorrectString = (w) => {
+    const getNewString = (s) => {
+        let result = '';
+        for(let i = 1; i < s.length - 1; i++) {
+            result += s[i] === '('? ')' : '(';
+        }
+        return result;
+    };
+    const updateString = (w) => {
         if(w === '') return '';
-        let str = '';
-        let [u, v] = separateString(w);
-        if(isCorrect(u)) {
-            str = `${u}${getCorrectString(v)}`;
+        const [u, v] = getBalancedStrings(w);
+        if(isRightString(u)) {
+            return u + updateString(v);
         } else {
-            u = u.slice(1, u.length - 1)
-                 .split('')
-                 .map(value => (value === '(' ? ')' : '('))
-                 .join('');
-            str = `(${getCorrectString(v)})${u}`;
+            return '(' + updateString(v) + ')' + getNewString(u);
         }
-        return str;
     };
     
-    return getCorrectString(p);
+    return updateString(p);
 }
