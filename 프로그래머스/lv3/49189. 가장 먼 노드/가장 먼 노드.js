efca0b4df7176b4bbcaa9e98e9b5bd21919e2e0c
Lani <laniiiiworld@gmail.com>
2023-07-14 11:53:48 +0900

    [level 3] Title: 가장 먼 노드, Time: 36.80 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,23 +1,50 @@
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.front = 0;
+        this.rear = 0;
+    }
+    size() {
+        return this.rear - this.front;
+    }
+    enqueue(item) {
+        this.queue.push(item);
+        this.rear += 1;
+    }
+    dequeue() {
+        const item = this.queue[this.front];
+        delete this.queue[this.front++];
+        return item;
+    }
+}
 function solution(n, edge) {
-    const distance = Array(n+1).fill(0);
-    distance[1] = 1;
-    
-    const gragh = Array.from({length: n+1}, () => []);
-    for(const [start, end] of edge) {
-        gragh[start].push(end);
-        gragh[end].push(start);
-    }
-    
-    const queue = [1];
-    while(queue.length) {
-        const current = queue.shift();
-        for(const node of gragh[current]) {
-            if(distance[node]) continue;
-            distance[node] = distance[current] + 1;
-            queue.push(node);
+    const distance = Array(n + 1).fill(Infinity);
+    const graph = Array.from({length: n + 1}, () => []);
+    const bfs = (start) => {
+        const queue = new Queue();
+        queue.enqueue([start, 0]);
+        distance[start] = 0;
+        
+        while(queue.size()) {
+            const [now, count] = queue.dequeue();
+            
+            for(const next of graph[now]) {
+                const calcDistance = count + 1;
+                if(distance[next] <= calcDistance) continue;
+                distance[next] = calcDistance;
+                queue.enqueue([next, calcDistance]);
+            }
         }
+    };
+    
+    for(const [start, end] of edge) {
+        graph[start].push(end);
+        graph[end].push(start);
     }
+    
+    distance[0] = 0;
+    bfs(1);
     
     const max = Math.max(...distance);
-    return distance.filter(value => value === max).length;
+    return distance.filter(v => v === max).length;
 }
