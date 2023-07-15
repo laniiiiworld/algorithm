3b769fc8f1cb050af7cd0c6bbb225ad42458688a
Lani <laniiiiworld@gmail.com>
2023-07-15 14:27:57 +0900

    [unrated] Title: 호텔 대실, Time: 2.44 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/README.md
+++ 프로그래머스/unrated/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [unrated] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
 
 ### 성능 요약
 
-메모리: 34.5 MB, 시간: 2.41 ms
+메모리: 34.6 MB, 시간: 2.44 ms
 
 ### 구분
 
--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.js
@@ -2,29 +2,27 @@ class MinHeap {
     constructor() {
         this.heap = [null];
     }
-    peek() {
-        return this.heap[1] || null;
-    }
     size() {
         return this.heap.length - 1;
     }
-    push(value) {
-        this.heap.push(value);
+    peek() {
+        return this.heap[1] || 24*60;
+    }
+    push(item) {
+        this.heap.push(item);
         
         let currentIndex = this.heap.length - 1;
-        let parentIndex = ~~(currentIndex / 2);
+        let parentIndex = Math.floor(currentIndex / 2);
         
-        while(parentIndex > 0 && this.heap[currentIndex] < this.heap[parentIndex]) {
+        while(parentIndex && this.heap[currentIndex] < this.heap[parentIndex]) {
             this._swap(currentIndex, parentIndex);
             currentIndex = parentIndex;
-            parentIndex = ~~(currentIndex / 2);
+            parentIndex = Math.floor(currentIndex / 2);
         }
     }
     pop() {
-        if(this.size() === 0) return null;
         if(this.size() === 1) return this.heap.pop();
-        
-        const returnValue = this.heap[1];
+        const returnItem = this.heap[1];
         this.heap[1] = this.heap.pop();
         
         let currentIndex = 1;
@@ -32,52 +30,48 @@ class MinHeap {
         let rightIndex = 3;
         
         if(this.size() === 1) {
-            return returnValue;
+            return returnItem;
         } else if(this.size() === 2) {
             if(this.heap[leftIndex] < this.heap[currentIndex]) {
                 this._swap(leftIndex, currentIndex);
             }
-            return returnValue;
+            return returnItem;
         }
         
         while((this.heap[leftIndex] < this.heap[currentIndex]) || 
               (this.heap[rightIndex] < this.heap[currentIndex])) {
-            const minIndex = (this.heap[leftIndex] <= this.heap[rightIndex])? leftIndex : rightIndex;
+            const minIndex = (this.heap[leftIndex] < this.heap[rightIndex])? leftIndex : rightIndex;
             this._swap(minIndex, currentIndex);
             currentIndex = minIndex;
             leftIndex = currentIndex * 2;
             rightIndex = currentIndex * 2 + 1;
+            
             if(leftIndex >= this.heap.length - 1) break;
         }
         
-        return returnValue;
+        return returnItem;
     }
     _swap(a, b) {
         [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
     }
 }
 function solution(book_time) {
-    const rooms = new MinHeap();
+    let answer = 0;
+    const heap = new MinHeap();
+    const getMinute = (time) => {
+        const [hh, mm] = time.split(':').map(Number);
+        return hh * 60 + mm;
+    };
+    const booked = book_time.map(([startTime, endTime]) => [getMinute(startTime), getMinute(endTime)])
+                            .sort((a, b) => a[0] - b[0] || a[1] - b[1]);
     
-    const bookTimes = book_time
-                        .map(booked => booked.map((time, index) => {
-                                const [hh24, mm] = time.split(':').map(Number);
-                                if(index === 0) { //startTime
-                                    return hh24 * 60 + mm;
-                                }
-                                return hh24 * 60 + mm + 10; //endTime
-                            }))
-                        .sort((a, b) => a[0] - b[0] || a[1] - b[1]);
-    
-    for(const [startTime, endTime] of bookTimes) {
-        const enabled = rooms.peek() || 0;
-        if(enabled > startTime) {
-            rooms.push(endTime);
-        } else {
-            rooms.pop();
-            rooms.push(endTime);
+    for(const [startTime, endTime] of booked) {
+        if(heap.peek() <= startTime) {
+            heap.pop();
         }
+        heap.push(endTime + 10);
+        answer = Math.max(answer, heap.size());
     }
     
-    return rooms.size();
+    return answer;
 }
