90830c6246745bf60a98014c1697270e44f20632
Lani <laniiiiworld@gmail.com>
2023-07-15 16:40:32 +0900

    [level 3] Title: 순위, Time: 19.56 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.js
+++ 프로그래머스/lv3/49191. 순위/순위.js
@@ -1,14 +1,14 @@
 function solution(n, results) {
     const graph = Array.from({length: n + 1}, () => Array(n + 1).fill(0));
-    
-    for(const [a, b] of results) {
-        graph[a][b] = 1;
-        graph[b][a] = -1;
+    for(const [won, lost] of results) {
+        graph[won][lost] = 1;
+        graph[lost][won] = -1;
     }
     
     for(let k = 1; k <= n; k++) {
         for(let a = 1; a <= n; a++) {
             for(let b = 1; b <= n; b++) {
+                if(graph[a][b]) continue;
                 if(graph[a][k] === 1 && graph[k][b] === 1) {
                     graph[a][b] = 1;
                 } else if(graph[a][k] === -1 && graph[k][b] === -1) {
