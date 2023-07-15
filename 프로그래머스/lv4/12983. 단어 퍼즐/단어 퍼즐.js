4d443b3673aeb07ad896f38f49b10e1d31998e1c
Lani <laniiiiworld@gmail.com>
2023-07-15 15:39:07 +0900

    [level 4] Title: 단어 퍼즐, Time: 28.48 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv4/12983. 단어 퍼즐/단어 퍼즐.js
+++ 프로그래머스/lv4/12983. 단어 퍼즐/단어 퍼즐.js
@@ -1,14 +1,15 @@
 function solution(strs, t) {
     const n = t.length;
-    const dp = new Array(n + 1).fill(Infinity);
+    const dp = Array(n + 1).fill(Infinity);
     dp[0] = 0;
     
-    for(let i = 0; i < n; i++) {
-        for(let j = Math.max(i - 5, 0); j <= i; j++) {
-            const str = t.slice(j, i + 1);
-            if(!strs.includes(str)) continue;
-            if(dp[i + 1] <= dp[j] + 1) continue;
-            dp[i + 1] = dp[j] + 1;
+    for(let i = 0; i <= n; i++) {
+        for(let j = i; j >= Math.max(i - 5, 0); j--) {
+            const text = t.slice(j, i);
+            
+            if(!strs.includes(text)) continue;
+            
+            dp[i] = Math.min(dp[i], dp[j] + 1);
         }
     }
     
