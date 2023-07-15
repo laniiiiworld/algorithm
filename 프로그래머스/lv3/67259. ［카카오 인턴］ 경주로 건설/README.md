bfe3a7e18336c60e0b0b857a0573f566954f84d7
Lani <laniiiiworld@gmail.com>
2023-07-15 20:42:53 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.78 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 0.72 ms
+메모리: 33.7 MB, 시간: 0.78 ms
 
 ### 구분
 
--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -9,16 +9,17 @@ class MinHeap {
         this.heap.push(item);
         
         let currentIndex = this.heap.length - 1;
-        let parentIndex = ~~(currentIndex / 2);
+        let parentIndex = Math.floor(currentIndex / 2);
         
-        while(parentIndex >= 1 && this.heap[currentIndex][2] < this.heap[parentIndex][2]) {
+        while(parentIndex && this.heap[currentIndex][2] < this.heap[parentIndex][2]) {
             this._swap(currentIndex, parentIndex);
             currentIndex = parentIndex;
-            parentIndex = ~~(currentIndex / 2);
+            parentIndex = Math.floor(currentIndex / 2);
         }
     }
     pop() {
-        if(this.heap.length === 2) return this.heap.pop();
+        if(this.size() === 1) return this.heap.pop();
+        
         const returnItem = this.heap[1];
         this.heap[1] = this.heap.pop();
         
@@ -26,24 +27,23 @@ class MinHeap {
         let leftIndex = 2;
         let rightIndex = 3;
         
-        if(!this.heap[leftIndex]) {
+        if(this.size() === 1) {
             return returnItem;
-        } else if(!this.heap[rightIndex]) {
+        } else if(this.size() === 2) {
             if(this.heap[leftIndex][2] < this.heap[currentIndex][2]) {
                 this._swap(leftIndex, currentIndex);
             }
             return returnItem;
         }
         
-        while(
-              (this.heap[leftIndex][2] < this.heap[currentIndex][2]) || 
-              (this.heap[rightIndex][2] < this.heap[currentIndex][2])
-            ) {
-            const minIndex = (this.heap[leftIndex][2] <= this.heap[rightIndex][2])? leftIndex : rightIndex;
+        while((this.heap[leftIndex][2] < this.heap[currentIndex][2]) || 
+              (this.heap[rightIndex][2] < this.heap[currentIndex][2])) {
+            const minIndex = (this.heap[leftIndex][2] < this.heap[rightIndex][2])? leftIndex : rightIndex;
             this._swap(minIndex, currentIndex);
             currentIndex = minIndex;
             leftIndex = currentIndex * 2;
             rightIndex = currentIndex * 2 + 1;
+            
             if(leftIndex >= this.heap.length - 1) break;
         }
         
@@ -53,37 +53,41 @@ class MinHeap {
         [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
     }
 }
-
 function solution(board) {
-    const N = board.length;
-    const distance = Array.from({length: N}, () => Array.from({length: N}, () => [Infinity, Infinity]));
-    const coordinates = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-    
-    const heap = new MinHeap();
-    heap.push([0, 0, 0, true]);
-    
-    while(heap.size()) {
-        const [y, x, cost, isNow] = heap.pop();
+    let answer = Infinity;
+    const n = board.length;
+    const newBoard = Array.from({length: n}, (_, i) => {
+        return Array.from({length: n}, (_, j) => board[i][j] === 1? [1, 1] : [Infinity, Infinity]);
+    });
+    const canMove = (y, x, d) => y >= 0 && y < n && x >= 0 && x < n && newBoard[y][x][d] !== 1;
+    //direction : 9 (방향없음), 0(상하), 1(좌우)
+    const bfs = (y, x, cost, direction) => {
+        const heap = new MinHeap();
+        heap.push([y, x, cost, direction]);
+        newBoard[y][x][0] = 0;
+        newBoard[y][x][1] = 0;
         
-        if(distance[y][x][Number(isNow)] < cost) continue;
-        distance[y][x][Number(isNow)] = cost;
-        
-        if(y === N - 1 && x === N - 1) break;
-        
-        for(const [plusY, plusX] of coordinates) {
-            const nextX = x + plusX;
-            const nextY = y + plusY;
-            if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) continue;
-            if(board[nextY][nextX]) continue;
-            
-            const isNext = plusY === 0;
-            const calculatedCost = ((y === 0 && x === 0) || isNow === isNext)? cost + 100 : cost + 600;
-            
-            if(distance[nextY][nextX][Number(isNext)] < calculatedCost) continue;
-            
-            heap.push([nextY, nextX, calculatedCost, isNext]);
+        while(heap.size()) {
+            const [nowY, nowX, nowCost, nowDirection] = heap.pop();
+            if(nowY === n - 1 && nowX === n - 1) {
+                answer = Math.min(answer, nowCost);
+            }
+
+            for(const [plusY, plusX] of [[-1, 0], [0, 1], [1, 0], [0, -1]]) {
+                const nextX = nowX + plusX;
+                const nextY = nowY + plusY;
+                const nextDirection = plusY? 0 : 1;
+                if(!canMove(nextY, nextX, nextDirection)) continue;
+                const plusCost = (nowDirection === 9 || nowDirection === nextDirection)? 100 : 600;
+                const calcCost = nowCost + plusCost;
+                if(newBoard[nextY][nextX][nextDirection] < calcCost) continue;
+                newBoard[nextY][nextX][nextDirection] = calcCost;
+                heap.push([nextY, nextX, calcCost, nextDirection]);
+            }
         }
-    }
+    };
     
-    return Math.min(...distance[N - 1][N - 1]);
+    bfs(0, 0, 0, 9);
+    
+    return answer;
 }
