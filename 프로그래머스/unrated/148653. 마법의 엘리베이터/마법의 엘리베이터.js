15239c32a5aa24b8c33947c98653b9c8710eee0f
Lani <laniiiiworld@gmail.com>
2023-07-15 14:48:28 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,45 +1,17 @@
-class Queue {
-    constructor() {
-        this.queue = [];
-        this.front = 0;
-        this.rear = 0;
-    }
-    size() {
-        return this.rear - this.front;
-    }
-    enqueue(value) {
-        this.queue.push(value);
-        this.rear += 1;
-    }
-    dequeue() {
-        const value = this.queue[this.front];
-        delete this.queue[this.front++];
-        return value;
-    }
-}
-
 function solution(storey) {
     let answer = Infinity;
+    const queue = [[storey, 0]];
     
-    const visited = new Set();
-    const queue = new Queue();
-    queue.enqueue([storey, 0]);
-
-    while(queue.size()) {
-        const [now, count] = queue.dequeue();
-        const remainder = now % 10;
+    while(queue.length) {
+        const [now, count] = queue.shift();
+        const rest = now % 10;
+        const next = Math.floor(now / 10);
         
-        if(now === 0) {
-            answer = Math.min(answer, count);
-            continue;
-        }
-        
-        if(remainder) {
-            const next = ~~(now / 10);
-            queue.enqueue([next, count + remainder]);
-            if(now > 5) queue.enqueue([next + 1, count + 10 - remainder]);
+        if(next > 0) {
+            queue.push([next + 1, count - rest + 10]);
+            queue.push([next, count + rest]);
         } else {
-            queue.enqueue([now / 10, count]);
+            answer = Math.min(answer, count + rest, count - rest + 10 + 1);
         }
     }
     
