4b5b3638052a3adb12a7b2d5c092ff74e52fe962
Lani <laniiiiworld@gmail.com>
2023-07-19 13:00:50 +0900

    [level 2] Title: 하노이의 탑, Time: 5.24 ms, Memory: 49.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/README.md
+++ 프로그래머스/lv2/12946. 하노이의 탑/README.md
@@ -1,10 +1,10 @@
 # [level 2] 하노이의 탑 - 12946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946) 
 
 ### 성능 요약
 
-메모리: 48.6 MB, 시간: 5.10 ms
+메모리: 49.1 MB, 시간: 5.24 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,16 +1,16 @@
 function solution(n) {
     const answer = [];
-    const dfs = (n, start, to, mid) => {
+    const dfs = (n, start, mid, to) => {
         if(n == 1){
             answer.push([start, to]);
             return;
         }
-        dfs(n-1, start, mid, to); //1번 기둥의 n-1개를 2번을 거쳐 3번 -> 2번으로 이동
-        answer.push([start, to]); //n을 1번 -> 3번으로 이동
-        dfs(n-1, mid, to, start); //2번의 기둥의 n-1개를 1번을 거쳐 2번 -> 3번으로 이동
+        dfs(n - 1, start, to, mid);
+        answer.push([start, to]);
+        dfs(n-1, mid, start, to);
     };
     
-    dfs(n, 1, 3, 2);
+    dfs(n, 1, 2, 3);
     
     return answer;
 }
