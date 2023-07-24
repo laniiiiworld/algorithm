6f51e6ef5c2bf19525d901dc24035836ded71b5f
Lani <laniiiiworld@gmail.com>
2023-07-24 18:29:25 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/README.md
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 숫자 카드 나누기 - 135807 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.11 ms
+메모리: 33.5 MB, 시간: 0.09 ms
 
 ### 구분
 
--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,22 +1,11 @@
 function solution(arrayA, arrayB) {
-    const getGCD = (a, b) => {
-        while(a % b) {
-            [a, b] = [b, a % b];
-        }
-        return b;
-    };
+    const getGCD = (a, b) => (a % b)? getGCD(b, a % b) : b;
     const gcdA = arrayA.reduce((gcd, v) => gcd = getGCD(gcd, v));
     const gcdB = arrayB.reduce((gcd, v) => gcd = getGCD(gcd, v));
-    
-    if((gcdA === 1 && gcdB === 1) || gcdA === gcdB) return 0;
-    
-    if(gcdA > gcdB) {
-        if(arrayB.every(v => v % gcdA > 0)) return gcdA;
-        if(gcdB > 1 && arrayA.every(v => v % gcdB > 0)) return gcdB;
-    } else {
-        if(arrayA.every(v => v % gcdB > 0)) return gcdB;
-        if(gcdA > 1 && arrayB.every(v => v % gcdA > 0)) return gcdA;
-    }
-    
-    return 0;
+
+    if(gcdA === gcdB) return 0;
+    if(gcdA < gcdB && arrayA.some(v => (v % gcdB) === 0)) return 0;
+    if(gcdA > gcdB && arrayB.some(v => (v % gcdA) === 0)) return 0;
+
+    return Math.max(gcdA, gcdB);
 }
