6509ea5bdf6de2031f8a79118f2d7cbcbf16f351
Lani <laniiiiworld@gmail.com>
2023-07-26 20:41:14 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/131130. 혼자 놀기의 달인/README.md
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/README.md
@@ -1,10 +1,10 @@
 # [level 2] 혼자 놀기의 달인 - 131130 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131130#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131130?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.31 ms
+메모리: 33.6 MB, 시간: 0.32 ms
 
 ### 구분
 
--- 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.js
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.js
@@ -1,42 +1,23 @@
-class Queue {
-    constructor() {
-        this.queue = [];
-        this.front = 0;
-        this.rear = 0;
-    }
-    size() {
-        return this.rear - this.front;
-    }
-    enqueue(value) {
-        this.queue.push(value);
-        this.rear += 1;
-    }
-    dequeue() {
-        const value = this.queue[this.front];
-        delete this.queue[this.front++];
-        return value;
-    }
-}
 function solution(cards) {
-    const answer = [];
     const n = cards.length;
     const visited = Array(n + 1).fill(false);
+    const results = [];
     const bfs = (start) => {
-        let result = 0;
-        const queue = new Queue();
-        queue.enqueue(start);
+        let result = 1;
+        const queue = [];
+        queue.push([start, 1]);
         visited[start] = true;
         
-        while(queue.size()) {
-            const now = queue.dequeue();
+        while(queue.length) {
+            const [now, count] = queue.shift();
             const next = cards[now - 1];
             
-            result += 1;
+            result = Math.max(result, count);
             
-            if(visited[next]) break;
+            if(visited[next]) continue;
             visited[next] = true;
             
-            queue.enqueue(next);
+            queue.push([next, count + 1]);
         }
         
         return result;
@@ -44,12 +25,10 @@ function solution(cards) {
     
     for(let i = 1; i <= n; i++) {
         if(visited[i]) continue;
-        answer.push(bfs(cards[i - 1]));
+        results.push(bfs(i));
     }
     
-    if(answer.length === 1) return 0;
+    results.sort((a, b) => b - a);
     
-    answer.sort((a, b) => b - a);
-    
-    return answer[0] * answer[1];
+    return results.length === 1? 0 : results[0] * results[1];
 }
