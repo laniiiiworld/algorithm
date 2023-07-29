8fe465ec101d18b21537aebd1d626bc25436a457
Lani <laniiiiworld@gmail.com>
2023-07-29 10:23:20 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -5,22 +5,22 @@ class MinHeap {
     size() {
         return this.heap.length - 1;
     }
-    push(value) {
-        this.heap.push(value);
+    push(item) {
+        this.heap.push(item);
         
         let currentIndex = this.heap.length - 1;
-        let parentIndex = ~~(currentIndex / 2);
+        let parentIndex = Math.floor(currentIndex / 2);
         
-        while(parentIndex > 0 && this.heap[currentIndex][1] < this.heap[parentIndex][1]) {
+        while(parentIndex && this.heap[currentIndex][1] < this.heap[parentIndex][1]) {
             this._swap(currentIndex, parentIndex);
             currentIndex = parentIndex;
-            parentIndex = ~~(currentIndex / 2);
+            parentIndex = Math.floor(currentIndex / 2);
         }
     }
     pop() {
         if(this.size() === 1) return this.heap.pop();
         
-        const returnValue = this.heap[1];
+        const returnItem = this.heap[1];
         this.heap[1] = this.heap.pop();
         
         let currentIndex = 1;
@@ -28,15 +28,15 @@ class MinHeap {
         let rightIndex = 3;
         
         if(this.size() === 1) {
-            return returnValue;
+            return returnItem;
         } else if(this.size() === 2) {
             if(this.heap[leftIndex][1] < this.heap[currentIndex][1]) {
                 this._swap(leftIndex, currentIndex);
             }
-            return returnValue;
+            return returnItem;
         }
         
-        while((this.heap[leftIndex][1] < this.heap[currentIndex][1]) ||
+        while((this.heap[leftIndex][1] < this.heap[currentIndex][1]) || 
               (this.heap[rightIndex][1] < this.heap[currentIndex][1])) {
             const minIndex = (this.heap[leftIndex][1] <= this.heap[rightIndex][1])? leftIndex : rightIndex;
             this._swap(minIndex, currentIndex);
@@ -46,37 +46,38 @@ class MinHeap {
             if(leftIndex >= this.heap.length - 1) break;
         }
         
-        return returnValue;
+        return returnItem;
     }
     _swap(a, b) {
         [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
     }
 }
 function solution(jobs) {
-    jobs.sort((a, b) => b[0] - a[0] || b[1] - a[1]);
+    const n = jobs.length;
+    const disk = new MinHeap();
+    let totalTime = 0; //작업의 요청부터 종료까지 걸린 시간
+    let time = 0; // 현재 시간
+    let jobIndex = 0;
     
-    const length = jobs.length;
-    const heap = new MinHeap();
-    let totalWorkTime = 0;
-    let time = 0;
+    jobs.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+    time = jobs[0][0];
     
-    while(jobs.length || heap.size()) {
-        if(jobs.length && heap.size() === 0) {
-            const [requestTime, workTime] = jobs.pop();
-            time = Math.max(requestTime, time);
-            heap.push([requestTime, workTime]);
+    while(disk.size() || jobIndex < n) {
+        if(jobIndex < n) {
+            // 하드디스크가 작업을 수행하고 있지 않을 때에는 먼저 요청이 들어온 작업부터 처리
+            if(disk.size() === 0 && jobs[jobIndex][0] > time) {
+                time = jobs[jobIndex][0];
+            }
+            //추가로 요청이 들어온 작업들 disk에 넣기
+            while(jobIndex < n && jobs[jobIndex][0] <= time) {
+                disk.push(jobs[jobIndex++]);
+            }
         }
-        
-        while(jobs.length && jobs[jobs.length - 1][0] <= time) {
-            heap.push(jobs.pop());
-        }
-        
-        if(heap.size() === 0) continue;
-        
-        const [requestTime, workTime] = heap.pop();
-        totalWorkTime += time - requestTime + workTime;
+        //disk에서 작업을 하나 꺼내 처리
+        const [requestTime, workTime] = disk.pop();
+        totalTime += time - requestTime + workTime;
         time += workTime;
     }
     
-    return ~~(totalWorkTime / length);
+    return Math.floor(totalTime / n);
 }
