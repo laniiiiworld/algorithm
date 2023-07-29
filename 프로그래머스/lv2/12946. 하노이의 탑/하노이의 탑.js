cd093874f5f339344b79f8d03a0b4e4e6485eca7
Lani <laniiiiworld@gmail.com>
2023-07-29 10:45:16 +0900

    [level 2] Title: 하노이의 탑, Time: 5.55 ms, Memory: 49 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,16 +1,19 @@
 function solution(n) {
     const answer = [];
-    const dfs = (n, start, mid, to) => {
-        if(n == 1){
-            answer.push([start, to]);
+    const moveBlocks = (m, one, two, three) => {
+        if(m === 1) {
+            answer.push([one, three]);
             return;
         }
-        dfs(n - 1, start, to, mid);
-        answer.push([start, to]);
-        dfs(n-1, mid, start, to);
+        //m - 1개의 원판을 2번 기둥으로 이동
+        moveBlocks(m - 1, one, three, two);
+        //m번 원판을 3번 기둥으로 이동
+        answer.push([one, three]);
+        //m - 1개의 원판을 3번 기둥으로 이동
+        moveBlocks(m - 1, two, one, three);
     };
     
-    dfs(n, 1, 2, 3);
+    moveBlocks(n, 1, 2, 3);
     
     return answer;
 }
