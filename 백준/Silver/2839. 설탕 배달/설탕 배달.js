eda2ef1c868eb11be28e79e45d36677f618a9e4e
Lani <laniiiiworld@gmail.com>
2023-08-14 21:05:54 +0900

    [Silver IV] Title: 설탕 배달, Time: 132 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,20 +1,17 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const n = Number(input[0]);
 const getBagCount = (weight) => {
-    let count = Math.floor(weight / 5);
-    if(weight % 5 === 0) return count;
-    
-    let last = weight % 5;
-    while(count && last % 3) {
-        count -= 1;
-        last += 5;
+    let count = 0;
+
+    while(weight > 0) {
+        if(weight % 5 === 0) {
+            return count + weight / 5;
+        }
+        weight -= 3;
+        count += 1;
     }
     
-    if(last % 3 === 0) return count + last / 3;
-    if(weight % 3 === 0) return weight / 3;
-    
-    
-    return -1;
+    return (weight === 0)? count : -1;
 };
 
 console.log(getBagCount(n));
