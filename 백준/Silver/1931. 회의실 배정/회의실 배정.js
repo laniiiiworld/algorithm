4e90bc68b42e0977fb4a3f64464b18376edafb5b
Lani <laniiiiworld@gmail.com>
2023-08-16 10:15:48 +0900

    [Silver I] Title: 회의실 배정, Time: 420 ms, Memory: 53632 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -3,18 +3,16 @@ const n = Number(input[0]);
 const meetingList = input
                         .slice(1)
                         .map(item => item.split(' ').map(Number))
-                        .sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+                        .sort((a, b) => a[1] - b[1] || a[0] - b[0]);
 
 const answer = [];
 for(const [a, b] of meetingList) {
-    if(answer.length && b < answer[answer.length - 1][1]) {
-        while(answer.length && b < answer[answer.length - 1][1]) {
-            answer.pop();
-        }
+    if(answer.length === 0) {
         answer.push([a, b]);
         continue;
     }
-    if(answer.length && a < answer[answer.length - 1][1]) continue;
+    const [start, end] = answer[answer.length - 1];
+    if(a < end) continue;
     answer.push([a, b]);
 }
 
