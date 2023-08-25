17ca60b5ceecc603823cc92d0e9e527021540baa
Lani <laniiiiworld@gmail.com>
2023-08-25 23:03:51 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,10 +1,9 @@
 function solution(data, col, row_begin, row_end) {
-    let answer = 0;
-    
     data.sort((a, b) => a[col - 1] - b[col - 1] || b[0] - a[0]);
     
-    for(let i = row_begin; i <=row_end; i++) {
-        answer ^= data[i - 1].reduce((acc, cur) => acc += cur % i, 0);
+    let answer = 0;
+    for(let i = row_begin - 1; i < row_end; i++) {
+        answer ^= data[i].reduce((acc, cur) => acc += cur % (i + 1), 0);
     }
     
     return answer;
