6cf6579b0d2fcd8954b40ed3378a2bd0c49de1ce
Lani <laniiiiworld@gmail.com>
2023-08-26 00:02:41 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 2.85 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/lv3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.js
+++ 프로그래머스/lv3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.js
@@ -2,21 +2,27 @@ function solution(n, cores) {
     let left = 0;
     let right = Math.max(...cores) * n;
     let time = 0;
-    let m = 0;
-    
-    while(left <= right) {
+    let m = 0; //time까지 처리한 작업량
+    const getWorkCount = (time, cores) => {
+        if(time === 0) {
+            return cores.length;
+        }
+        return cores.reduce((acc, core) => acc += Math.floor(time / core), cores.length);
+    };
+
+    while (left <= right) {  
         const mid = Math.floor((left + right) / 2);
-        const count = (mid === 0)? cores.length : cores.reduce((acc, cur) => acc += Math.floor(mid / cur), cores.length);
-        
-        if(count >= n) {
-            right = mid - 1;
-            time = mid;
-            m = count;
+        const count = getWorkCount(mid, cores);
+
+        if (count >= n) {
+            right = mid - 1;   
+            time = mid;     
+            m = count; 
         } else {
             left = mid + 1;
         }
     }
-    
+
     let answer = cores.length;
     while(answer--) {
         if (time % cores[answer] > 0) continue;
