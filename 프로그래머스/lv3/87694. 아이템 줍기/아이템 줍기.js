216ac29ab809a889729ccd9b33d4b896cad9c1bb
Lani <laniiiiworld@gmail.com>
2023-08-28 22:35:52 +0900

    [level 3] Title: 아이템 줍기, Time: 0.57 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.js
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.js
@@ -1,50 +1,37 @@
+const directions = [[0, 0], [0, -1], [-1, -1], [-1, 0]];
+const moving = [[1, 0], [0, -1], [-1, 0], [0, 1]];
+
 function solution(rectangle, characterX, characterY, itemX, itemY) {
-    let answer = Infinity;
-    const N = Math.max(...rectangle.map(item => Math.max(...item))) * 2;
-    // 0 : field 외부
-    // 1 : rectangle 테두리
-    // -1: rectangle 내부
-    // 2 : 지나간 곳
-    const field = Array.from({length: N+1}, () => Array(N+1).fill(0));
-    
-    rectangle.forEach(([x1, y1, x2, y2]) => {
-        x1 *= 2;
-        y1 *= 2;
-        x2 *= 2;
-        y2 *= 2;
-        
-        // 사각형 테두리
-        for(let i=x1; i<=x2; i++) {
-            if(field[y1][i] > -1) field[y1][i] = 1;
-            if(field[y2][i] > -1) field[y2][i] = 1;
-        }
-        for(let j=y1; j<=y2; j++) {
-            if(field[j][x1] > -1) field[j][x1] = 1;
-            if(field[j][x2] > -1) field[j][x2] = 1;
-        }
-        // 사각형 내부
-        for(let i=x1+1; i<x2; i++) {
-            for(let j=y1+1; j<y2; j++) {
-                field[j][i] = -1;
+    const pointSet = new Set();
+    rectangle.forEach(v => {
+        const [x1, y1, x2, y2] = v;
+        for (let i = x1; i < x2; i++) {
+            for (let j = y1; j < y2; j++) {
+                pointSet.add(i + ',' + j);
             }
         }
     });
-    
-    const moveCharacter = (x, y, count) => {
-        if(x === itemX*2 && y === itemY*2) {
-            answer = Math.min(answer, count);
-            return;
+    const move = (position) => {
+        for (let i = 0; i <= 4; i++) {
+            const prevBlockPosition = directions[i].map((v, i) => v + position[i]).join(',');
+            const nextBlockPosition = directions[(i+1)%4].map((v, i) => v + position[i]).join(',');
+            if (
+                !pointSet.has(prevBlockPosition) && 
+                pointSet.has(nextBlockPosition)
+            ) return position.map((v, j) => v + moving[i][j])
         }
-        
-        field[y][x] = 2;
-        
-        if(x+1 <= N && field[y][x+1] === 1) {moveCharacter(x+1, y, count+1);}
-        if(y+1 <= N && field[y+1][x] === 1) {moveCharacter(x, y+1, count+1);}
-        if(x-1 >= 0 && field[y][x-1] === 1) {moveCharacter(x-1, y, count+1);}
-        if(y-1 >= 0 && field[y-1][x] === 1) {moveCharacter(x, y-1, count+1);}
-    };
-        
-    moveCharacter(characterX*2, characterY*2, 0);
-    
-    return answer/2;
+    }
+    let clockWiseDistance = 0, reverseDistance = 0;
+    let position = [characterX, characterY];
+    while(position[0] != itemX || position[1] != itemY) {
+        position = move(position);
+        clockWiseDistance += 1;
+    }
+    while(position[0] != characterX || position[1] != characterY) {
+        position = move(position);
+        reverseDistance += 1;
+    }
+
+
+    return Math.min(clockWiseDistance, reverseDistance);
 }
