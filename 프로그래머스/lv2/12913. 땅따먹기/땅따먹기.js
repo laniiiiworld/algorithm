9febaf7fdce37a9b1a041b28c9f301d762d5e1e3
Lani <laniiiiworld@gmail.com>
2023-09-08 11:09:02 +0900

    [level 2] Title: 땅따먹기, Time: 36.36 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -1,14 +1,13 @@
 function solution(land) {
-    const sums = land.reduce((acc, row) => {
-        const [sumA, sumB, sumC, sumD] = acc;
-        const [a, b, c, d] = row;
-        return [
-                a + Math.max(sumB, sumC, sumD),
-                b + Math.max(sumA, sumC, sumD),
-                c + Math.max(sumA, sumB, sumD),
-                d + Math.max(sumA, sumB, sumC)
-               ];
-    }, [0, 0, 0, 0]);
-    
-    return Math.max(...sums);
+    return Math.max(...land.reduce((acc, cur) => {
+                            const [sumA, sumB, sumC, sumD] = acc;
+                            const [a, b, c, d] = cur;
+                            return [
+                                Math.max(sumB, sumC, sumD) + a,
+                                Math.max(sumA, sumC, sumD) + b,
+                                Math.max(sumA, sumB, sumD) + c,
+                                Math.max(sumA, sumB, sumC) + d,
+                            ];
+                        }, [0, 0, 0, 0])
+                   );
 }
