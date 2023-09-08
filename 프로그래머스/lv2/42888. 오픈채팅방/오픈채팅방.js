dfceb64fa007eecf9f46d1e07a4384eee4035c7e
Lani <laniiiiworld@gmail.com>
2023-09-08 20:46:23 +0900

    [level 2] Title: 오픈채팅방, Time: 155.94 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,19 +1,20 @@
 function solution(record) {
+    const answer = [];
+    const friends = new Map();
     const messages = {
-                        'Enter': '님이 들어왔습니다.', 
-                        'Leave': '님이 나갔습니다.'
-                     };
-    const result = [];
-    const user = new Map();
+        Enter: '님이 들어왔습니다.',
+        Leave: '님이 나갔습니다.'
+    };
     
-    for(const text of record) {
-        const [type, id, name] = text.split(' ');
-        
-        if(type !== 'Leave') user.set(id, name);
-        if(type === 'Change') continue;
-        
-        result.push([id, messages[type]]);
+    for(const row of record) {
+        const [type, id, name] = row.split(' ');
+        if(type !== 'Change') {
+            answer.push([type, id]);
+        }
+        if(type !== 'Leave') {
+            friends.set(id, name);
+        }
     }
     
-    return result.map(([id, message]) => `${user.get(id)}${message}`);
+    return answer.map(([type, id]) => `${friends.get(id)}${messages[type]}`);
 }
