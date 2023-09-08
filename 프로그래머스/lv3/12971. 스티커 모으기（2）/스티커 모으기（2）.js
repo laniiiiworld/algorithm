71db767bd707b4a02d1b56b1f5085bf571ff689e
Lani <laniiiiworld@gmail.com>
2023-09-08 22:59:50 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.11 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -2,16 +2,14 @@ function solution(sticker) {
     const n = sticker.length;
     
     if(n === 1) return sticker[0];
-    if(n === 2) return Math.max(sticker[0], sticker[1]);
     
     const dp1 = [sticker[0], Math.max(sticker[0], sticker[1])];
     const dp2 = [0, sticker[1]];
     
     for(let i = 2; i < n; i++) {
-        if(i < n - 1) {
-            dp1.push(Math.max(dp1[i - 2] + sticker[i], dp1[i - 1]));
-        }
         dp2.push(Math.max(dp2[i - 2] + sticker[i], dp2[i - 1]));
+        if(i === n - 1) continue;
+        dp1.push(Math.max(dp1[i - 2] + sticker[i], dp1[i - 1]));
     }
     
     return Math.max(dp1[n - 2], dp2[n - 1]);
