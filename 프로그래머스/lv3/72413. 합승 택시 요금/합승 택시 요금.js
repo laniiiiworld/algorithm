95461161bffb6e52f27ed5e20bd4cf198aa79750
Lani <laniiiiworld@gmail.com>
2023-09-09 13:13:49 +0900

    [level 3] Title: 합승 택시 요금, Time: 11.02 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,29 +1,26 @@
 function solution(n, s, a, b, fares) {
-    const graph = Array.from({length: n + 1}, (_, i) => {
-        return Array.from({length: n + 1}, (_, j) => (i === j)? 0 : Infinity);
-    });
+    const graph = Array.from({length: n + 1}, (_, i) => Array.from({length: n + 1}, (_, j) => (i === j)? 0 : Infinity));
     
-    for(const [c, d, f] of fares) {
-        graph[c][d] = f;
-        graph[d][c] = f;
+    for(const [start, end, fee] of fares) {
+        graph[start][end] = fee;
+        graph[end][start] = fee;
     }
     
-    for(let mid = 1; mid <= n; mid++) {
-        for(let start = 1; start <= n; start++) {
-            for(let end = 1; end <= n; end++) {
-                const calcFee = graph[start][mid] + graph[mid][end];
-                if(graph[start][end] <= calcFee) continue;
-                graph[start][end] = calcFee;
+    for(let k = 1; k <= n; k++) {
+        for(let i = 1; i <= n; i++) {
+            for(let j = 1; j <= n; j++) {
+                const calculated = graph[i][k] + graph[k][j];
+                if(graph[i][j] <= calculated) continue;
+                graph[i][j] = calculated;
             }
         }
     }
     
-    let fee = graph[s][a] + graph[s][b];
-    for(let share = 1; share <= n; share++) {
-        const calcFee = graph[s][share] + graph[share][a] + graph[share][b];
-        if(fee <= calcFee) continue;
-        fee = calcFee;
+    let answer = Infinity;
+    for(let mid = 1; mid <= n; mid++) {
+        const calculated = graph[s][mid] + graph[mid][a] + graph[mid][b];
+        if(answer <= calculated) continue;
+        answer = calculated;
     }
-    
-    return fee;
+    return answer;
 }
