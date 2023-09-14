e2727c35c05943757f30d13e461a17530234cb51
Lani <laniiiiworld@gmail.com>
2023-09-14 22:33:12 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.11 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.js
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -1,11 +1,9 @@
 function solution(n) {
-    const answer = [];
-    const base = [4, 1, 2];
-    
-    while(n >= 1) {
-        answer.push(base[n % 3]);
-        n = n % 3? Math.floor(n / 3) : Math.floor((n - 1) / 3);
+    let answer = '';
+    const rule = ['4', '1', '2'];
+    while(n > 3){
+        answer = rule[n % 3] + answer;
+        n = (n % 3)? Math.floor(n / 3) : Math.floor(n / 3) - 1;
     }
-    
-    return answer.reverse().join('');
+    return rule[n % 3] + answer;
 }
