24a9a38d01ff8b7c4ee8092c54c8f6e9568351cc
Lani <laniiiiworld@gmail.com>
2023-09-22 22:13:25 +0900

    [unrated] Title: 미로 탈출, Time: 0.46 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/README.md
+++ 프로그래머스/unrated/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [unrated] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.54 ms
+메모리: 33.5 MB, 시간: 0.46 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.js
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.js
@@ -1,15 +1,15 @@
+
 class Queue {
     constructor() {
         this.queue = [];
         this.front = 0;
         this.rear = 0;
     }
-    size() {
-        return this.rear - this.front;
+    isEmpty() {
+        return this.rear === this.front;
     }
     enqueue(value) {
-        this.queue.push(value);
-        this.rear += 1;
+        this.queue[this.rear++] = value;
     }
     dequeue() {
         const value = this.queue[this.front];
@@ -19,49 +19,45 @@ class Queue {
 }
 
 function solution(maps) {
-    const moves = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-    const n = maps.length;
-    const m = maps[0].length;
-    const getStartPoint = (point) => {
-        for(let i = 0; i < n; i++) {
-            for(let j = 0; j < m; j++) {
-                if(maps[i][j] === point) return [i, j];
+    const MAX_R = maps.length;
+    const MAX_C = maps[0].length;
+    const dx = [1, 0, -1, 0];
+    const dy = [0, 1, 0, -1];
+    let start = [0, 0];
+    let queue = new Queue();
+    let visited = maps.map((v, row) => {
+        if (v.includes('S')) {
+            const col = v.indexOf('S');
+            start = [row, col];
+        }
+        return Array(v.length).fill(false);
+    });
+
+    visited[start[0]][start[1]] = true;
+    queue.enqueue({ row: start[0], col: start[1], length: 0, open: false });
+
+
+    while (!queue.isEmpty()) {
+        let { row, col, length, open } = queue.dequeue();
+        if (maps[row][col] === 'L') {
+            visited = maps.map(v => Array(v.length).fill(false));
+            visited[row][col] = true;
+            open = true;
+            queue = new Queue();
+        }
+        if (open && maps[row][col] === 'E') {
+            return length;
+        }
+
+        for (let i = 0; i < 4; i++) {
+            const nr = row + dy[i];
+            const nc = col + dx[i];
+            if (nr > -1 && nc > -1 && nr < MAX_R && nc < MAX_C && !visited[nr][nc] && maps[nr][nc] !== 'X') {
+                visited[nr][nc] = true;
+                queue.enqueue({ row: nr, col: nc, length: length + 1, open });
             }
         }
-    };
-    const bfs = (row, col, end) => {
-        const visited = Array.from({length: n}, () => Array(m).fill(false));
-        let result = n * m + 1;
-        const queue = new Queue();
-        queue.enqueue([row, col, 0]);
-        
-        while(queue.size()) {
-            const [y, x, count] = queue.dequeue();
-            if(maps[y][x] === end) {
-                result = Math.min(result, count);
-                continue;
-            }
-            for(const [plusY, plusX] of moves) {
-                const nextY = y + plusY;
-                const nextX = x + plusX;
-                if(nextX < 0 || nextY < 0 || nextX >= m || nextY >= n) continue;
-                if(maps[nextY][nextX] === 'X' || visited[nextY][nextX]) continue;
-                visited[nextY][nextX] = true;
-                queue.enqueue([nextY, nextX, count + 1]);
-            }
-        }
-        
-        return result === n * m + 1? 0 : result;
-    };
-    
-    let answer = 0;
-    for(const [start, end] of [['S', 'L'], ['L', 'E']]){
-        const [row, col] = getStartPoint(start);
-        const seconds = bfs(row, col, end);
-        
-        if(seconds === 0) return -1;
-        
-        answer += seconds;
     }
-    return answer;
+
+    return -1;
 }
