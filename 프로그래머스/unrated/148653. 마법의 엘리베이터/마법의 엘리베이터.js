66d495535d3fd7f2a8e7285226c7f759a491f3df
Lani <laniiiiworld@gmail.com>
2023-09-23 20:21:54 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,19 +1,6 @@
 function solution(storey) {
-    let answer = Infinity;
-    const queue = [[storey, 0]];
-    
-    while(queue.length) {
-        const [now, count] = queue.shift();
-        const rest = now % 10;
-        const next = Math.floor(now / 10);
-        
-        if(next > 0) {
-            queue.push([next + 1, count - rest + 10]);
-            queue.push([next, count + rest]);
-        } else {
-            answer = Math.min(answer, count + rest, count - rest + 10 + 1);
-        }
-    }
-    
-    return answer;
+    if (storey < 5) return storey;
+    const r = storey % 10;
+    const m = (storey - r) / 10;
+    return Math.min(r + solution(m), 10 - r + solution(m + 1));
 }
