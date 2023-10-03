ed58fd01604cec81a38fa99b662f3bf93ae1e44a
Lani <laniiiiworld@gmail.com>
2023-10-03 22:02:35 +0900

    [unrated] Title: 요격 시스템, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.js
@@ -1,18 +1,17 @@
 function solution(targets) {
-    let answer = 1;
-    targets.sort((a, b) => a[1] - b[1]);
-    targets.sort((a, b) => a[0] - b[0]);
-
-    let e = targets[0][1];
-
-    for(const i of targets) {
-        if(e > i[0]) {
-            e = Math.min(i[1], e);
-        } else {
+    let answer = 0;
+    let tail = -1;
+    
+    targets.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+    
+    for(const [s, e] of targets) {
+        if (tail <= s) {
             answer++;
-            e = i[1];
+            tail = e;
+        } else if (e < tail) {
+            tail = e;
         }
     }
-
+    
     return answer;
 }
