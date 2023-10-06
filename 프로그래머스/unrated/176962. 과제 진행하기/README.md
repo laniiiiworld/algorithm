2ba10f6e6f347a8dcbcd85ec3e80fd60f78299b6
Lani <laniiiiworld@gmail.com>
2023-10-06 20:48:22 +0900

    [unrated] Title: 과제 진행하기, Time: 7.89 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/README.md
+++ 프로그래머스/unrated/176962. 과제 진행하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 과제 진행하기 - 176962 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176962#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176962?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.2 MB, 시간: 10.10 ms
+메모리: 37.7 MB, 시간: 7.89 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.js
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.js
@@ -1,102 +1,38 @@
-class MinHeap {
-    constructor() {
-        this.heap = [null];
-    }
-    peek() {
-        return this.heap[1];
-    }
-    size() {
-        return this.heap.length - 1;
-    }
-    push(item) {
-        this.heap.push(item);
-        
-        let currentIndex = this.heap.length - 1;
-        let parentIndex = Math.floor(currentIndex / 2);
-        
-        while(parentIndex > 0 && this.heap[currentIndex].start < this.heap[parentIndex].start) {
-            this._swap(currentIndex, parentIndex);
-            currentIndex = parentIndex;
-            parentIndex = Math.floor(currentIndex / 2);
-        }
-    }
-    pop() {
-        if(this.size() === 1) return this.heap.pop();
-        
-        const returnItem = this.heap[1];
-        this.heap[1] = this.heap.pop();
-        
-        let currentIndex = 1;
-        let leftIndex = 2;
-        let rightIndex = 3;
-        
-        if(this.size() === 1) {
-            return returnItem;
-        } else if(this.size() === 2) {
-            if(this.heap[leftIndex].start < this.heap[currentIndex].start) {
-                this._swap(leftIndex, currentIndex);
-            }
-            return returnItem;
-        }
-        
-        while((this.heap[leftIndex].start < this.heap[currentIndex].start) || 
-              (this.heap[rightIndex].start < this.heap[currentIndex].start)) {
-            const minIndex = (this.heap[leftIndex].start <= this.heap[rightIndex].start)? leftIndex : rightIndex;
-            this._swap(minIndex, currentIndex);
-            currentIndex = minIndex;
-            leftIndex = currentIndex * 2;
-            rightIndex = currentIndex * 2 + 1;
-            if(leftIndex >= this.heap.length - 1) break;
-        }
-        
-        return returnItem;
-    }
-    _swap(a, b) {
-        [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
-    }
-}
 function solution(plans) {
-    let time = 0;
     const answer = [];
-    const plansHeap = new MinHeap();
-    const remained = new MinHeap();
-    const getPlayTime = (name, start, playTime) => {
-        if(plansHeap.size()) {
-            playTime = playTime - (plansHeap.peek().start - time);
-            time = (playTime < 0)? plansHeap.peek().start + playTime : plansHeap.peek().start;
-        } else {
-            playTime = 0;
-        }
-        return playTime;
-    };
-    const workPlane = (heap) => {
-        let {name, start, playTime} = heap.pop();
-        
-        const played = getPlayTime(name, start, playTime);
-        
-        if(played > 0) {
-            remained.push({name, start: -1 * time, playTime: played});
-        } else {
-            answer.push(name);
-        }
+    const hash = {};
+    const convertTime = (str) => {
+        let [h,m] = str.split(':').map(Number);
+        return h * 60 + m;
     };
     
-    for(const [name, start, playTime] of plans) {
-        const [hh24, mm] = start.split(':').map(Number);
-        plansHeap.push({name, start: hh24 * 60 + mm, playTime: Number(playTime)});
-    }
+    plans.forEach(el => {
+        el[1] = convertTime(el[1]);
+        el[2] = Number(el[2]);
+
+        hash[el[1]] = [el[2], el[0]];
+    })
+
+    plans.sort((a, b) => a[1] - b[1]);
+
+    const stack =[];
+    let startTime = plans[0][1];
+    let finish = 0;
     
-    time = plansHeap.peek().start;
-    
-    while(plansHeap.size() || remained.size()) {
-        if(plansHeap.size() && plansHeap.peek().start <= time) {
-            workPlane(plansHeap);
-        } else if(remained.size() && remained.peek().start <= time) {
-            workPlane(remained);
-        } else if(plansHeap.size() && plansHeap.peek().start > time) {
-            time = plansHeap.peek().start;
+    while(finish < plans.length) {
+        if(hash[startTime]) {
+            stack.push(hash[startTime]);
         }
+        if(stack.length) {
+            stack[stack.length-1][0]--;
+            if(stack[stack.length-1][0] === 0) {
+                answer.push(stack[stack.length-1][1]);
+                stack.pop();
+                finish++;
+            }
+        }
+        startTime++;
     }
-    
+
     return answer;
 }
